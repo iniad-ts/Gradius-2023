2c3a9fd93056dfc1e81fa5b76819f774c689597f
s1f10230014 <s1f102300144@iniad.org>
2023-07-07 10:40:12 +0900

    test

--- src/konva/konva.tsx
+++ src/konva/konva.tsx
@@ -2,9 +2,10 @@ import { Layer, Rect, Stage } from 'react-konva';
 
 function App() {
   return (
-    <Stage width={500} height={500}>
+    <Stage width={2200} height={900}>
       <Layer>
-        <Rect fill="red" width={200} height={200} x={0} y={0} />
+        <Rect fill="red" width={200} height={200} x={100} y={0} />
+        <Rect fill="blue" x={900} y={400} width={200} height={200} />
       </Layer>
     </Stage>
   );
---
+++ src/pages/gradius/index.tsx
@@ -0,0 +1,11 @@
+import styles from '/index.module.css';
+
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.hoge}>ここにゲーム画面を</div>
+    </div>
+  );
+};
+
+export default Home;
