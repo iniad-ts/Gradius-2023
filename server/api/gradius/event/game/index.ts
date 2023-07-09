e849224d752236ee047119e5c1bd50895a517efd
1f10230234 <s1f102302348@iniad.org>
2023-07-09 11:19:49 +0900

    謎2

--- server/api/gradius/event/game/index.ts
+++ server/api/gradius/event/game/index.ts
@@ -2,9 +2,7 @@ import type { EventModel, GameModel } from '$/commonTypesWithClient/models';
 
 export type Methods = {
   post: {
-    post: {
-      reqBody: string;
-      resBody: { game: GameModel; event: EventModel };
-    };
+    reqBody: string;
+    resBody: { game: GameModel; event: EventModel };
   };
 };
