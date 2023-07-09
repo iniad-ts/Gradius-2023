2323e0d45ac0ec9627caf92a2617e71b1f216e6e
s1f10230014 <s1f102300144@iniad.org>
2023-07-09 13:50:23 +0900

    gradius_game_screeのusestateを2個から１個に変更

--- src/pages/gradius_game_screen/index.page.tsx
+++ src/pages/gradius_game_screen/index.page.tsx
@@ -1,17 +1,14 @@
 //ここにゲーム画面をつくる
-
 import { useEffect, useState } from 'react';
 import { Layer, Rect, Stage } from 'react-konva';
 import { Loading } from 'src/components/Loading/Loading';
 import { apiClient } from 'src/utils/apiClient';
 
 const App = () => {
-  const [x_of_fighter, setx_of_fighter] = useState(0);
-  const [y_of_fighter, sety_of_fighter] = useState(0);
+  const [fight_position, setfight_position] = useState([0, 0]);
   const fetchBord = async () => {
     const new_fighter_position = await apiClient.game_screen.$get();
-    setx_of_fighter(new_fighter_position[0]);
-    sety_of_fighter(new_fighter_position[1]);
+    setfight_position(new_fighter_position);
   };
   useEffect(() => {
     const cancellid = setInterval(fetchBord, 100);
@@ -19,14 +16,13 @@ const App = () => {
       clearInterval(cancellid);
     };
   }, []);
-  if (!x_of_fighter || !y_of_fighter) return <Loading visible />;
+  if (!fight_position) return <Loading visible />;
   return (
     <Stage width={1100} height={690}>
       <Layer>
-        <Rect fill="red" width={100} height={40} x={x_of_fighter} y={y_of_fighter} />
+        <Rect fill="red" width={100} height={40} x={fight_position[0]} y={fight_position[1]} />
       </Layer>
     </Stage>
   );
 };
-
 export default App;
