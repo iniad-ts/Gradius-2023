1b1de8e39f63ad147687bff7849e343180344256
rarandeyo <s1F102301392@iniad.org>
2023-07-10 21:28:49 +0900

    玉排出機構

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useAtom } from 'jotai';
 import { useEffect, useState } from 'react';
-import { Layer, Rect, Stage } from 'react-konva';
+import { Circle, Layer, Rect, Stage } from 'react-konva';
 import { Loading } from 'src/components/Loading/Loading';
 import { apiClient } from 'src/utils/apiClient';
 import { userAtom } from '../atoms/user';
@@ -10,6 +10,7 @@ const Home = () => {
   const [user] = useAtom(userAtom);
   const [nowkey, setNowkey] = useState([0, 0]);
   const [enemyY, setEnemyY] = useState(300);
+  const [gradius_bullet, setGradius_bullet] = useState([nowkey[1], nowkey[0]]);
 
   const keyDownHandler = async (e: React.KeyboardEvent<HTMLDivElement>) => {
     const a = await apiClient.control.post({
@@ -25,12 +26,18 @@ const Home = () => {
       setEnemyY(got);
     }
   };
+  const updateGradiusBullet = async () => {
+    setGradius_bullet((prev) => [prev[0], prev[1] + 10]);
+  };
 
   useEffect(() => {
+    const timer = setInterval(updateGradiusBullet, 50);
+
     const cancelid = setInterval(updateEnemyY, 50);
 
     return () => {
       clearInterval(cancelid);
+      clearInterval(timer);
     };
   }, [enemyY]);
 
@@ -47,6 +54,7 @@ const Home = () => {
           <Layer>
             <Rect x={nowkey[1]} y={nowkey[0]} width={50} height={40} fill="blue" />
             <Rect x={1100} y={enemyY} width={50} height={40} fill="red" />
+            <Circle x={gradius_bullet[1]} y={gradius_bullet[0]} radius={10} fill="green" />
           </Layer>
         </Stage>
       </div>
