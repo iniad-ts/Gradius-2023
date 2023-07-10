b6bc68cc59ecaf5a2a0ad8b6263d7827ed5bf64c
s1f102003425 <s1f102003425@iniad.org>
2023-07-10 23:48:55 +0900

    konvaの導入+戦闘機もどきを作成

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,76 +1,88 @@
-import type { TaskModel } from '$/commonTypesWithClient/models';
-import { useAtom } from 'jotai';
-import type { ChangeEvent, FormEvent } from 'react';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import { Layer, Line, Rect, Stage } from 'react-konva';
 import { Loading } from 'src/components/Loading/Loading';
-import { BasicHeader } from 'src/pages/@components/BasicHeader/BasicHeader';
 import { apiClient } from 'src/utils/apiClient';
-import { returnNull } from 'src/utils/returnNull';
-import { userAtom } from '../atoms/user';
 import styles from './index.module.css';
 
 const Home = () => {
-  const [user] = useAtom(userAtom);
-  const [tasks, setTasks] = useState<TaskModel[]>();
-  const [label, setLabel] = useState('');
-  const inputLabel = (e: ChangeEvent<HTMLInputElement>) => {
-    setLabel(e.target.value);
-  };
-  const fetchTasks = async () => {
-    const tasks = await apiClient.tasks.$get().catch(returnNull);
+  //黒い枠の中をクリックし、矢印ボタンを押すと、赤い点が動くよー
+  const [playerX, setPlayerX] = useState(5);
+  const [playerY, setPlayerY] = useState(0);
+  const [board, setBoard] = useState([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+  const hoge = true;
+  const keydown = async (e: React.KeyboardEvent<HTMLDivElement>) => {
+    console.log(e);
+    console.log(e.code);
+    const game = await apiClient.game.$post({
+      body: { x: playerX, y: playerY, key: e.code, board },
+    });
 
-    if (tasks !== null) setTasks(tasks);
-  };
-  const createTask = async (e: FormEvent) => {
-    e.preventDefault();
-    if (!label) return;
-
-    await apiClient.tasks.post({ body: { label } });
-    setLabel('');
-    await fetchTasks();
-  };
-  const toggleDone = async (task: TaskModel) => {
-    await apiClient.tasks._taskId(task.id).patch({ body: { done: !task.done } });
-    await fetchTasks();
-  };
-  const deleteTask = async (task: TaskModel) => {
-    await apiClient.tasks._taskId(task.id).delete();
-    await fetchTasks();
+    setPlayerX(game.x);
+    setPlayerY(game.y);
+    setBoard(game.board);
+    console.log(game.board);
   };
 
-  useEffect(() => {
-    fetchTasks();
-  }, []);
+  const click = async (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    console.log(e);
+  };
+  //ここまで
 
-  if (!tasks || !user) return <Loading visible />;
+  if (!hoge) return <Loading visible />;
 
   return (
     <>
-      <BasicHeader user={user} />
-      <div className={styles.title} style={{ marginTop: '160px' }}>
-        Welcome to frourio!
+      <div
+        className="container"
+        onKeyDown={keydown}
+        style={{ border: 'solid' }}
+        onClick={click}
+        tabIndex={0}
+      >
+        <div id="key">X:{playerX}</div>
+        <div id="key">Y:{playerY}</div>
+        <div id="key" />
       </div>
-
-      <form style={{ textAlign: 'center', marginTop: '80px' }} onSubmit={createTask}>
-        <input value={label} type="text" onChange={inputLabel} />
-        <input type="submit" value="ADD" />
-      </form>
-      <ul className={styles.tasks}>
-        {tasks.map((task) => (
-          <li key={task.id}>
-            <label>
-              <input type="checkbox" checked={task.done} onChange={() => toggleDone(task)} />
-              <span>{task.label}</span>
-            </label>
-            <input
-              type="button"
-              value="DELETE"
-              className={styles.deleteBtn}
-              onClick={() => deleteTask(task)}
-            />
-          </li>
-        ))}
-      </ul>
+      <div
+        id="player"
+        style={{
+          position: 'absolute',
+          left: `${playerX}px`,
+          top: `${playerY}px`,
+          backgroundColor: 'red',
+          width: '10px',
+          height: '10px',
+        }}
+      />
+      <div className={styles.board}>
+        {board.map((row, y) =>
+          // eslint-disable-next-line complexity
+          row.map((color, x) => (
+            <div className={styles.cell} key={`${x}-${y}`} style={{ position: 'relative' }}>
+              {color !== 0 && (
+                <Stage width={40} height={40}>
+                  <Layer>
+                    <Rect fill="red" x={0} y={10} width={20} height={10} />
+                    <Line points={[20, 10, 20, 20, 40, 15, 20, 10]} stroke="white" fill="white" />
+                  </Layer>
+                </Stage>
+              )}
+            </div>
+          ))
+        )}
+      </div>
     </>
   );
 };
