f2066fe529383bbbc684ea5971bbb8ee3ace1fe0
1F10220146 <s1f102201469@iniad.org>
2023-07-14 02:57:09 +0900

    fix: typecheckエラー

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,26 +1,12 @@
-import type { TaskModel } from '$/commonTypesWithClient/models';
 import { useAtom } from 'jotai';
-import { useEffect, useState } from 'react';
 import { Loading } from 'src/components/Loading/Loading';
 import { App } from 'src/test/test';
-import { apiClient } from 'src/utils/apiClient';
-import { returnNull } from 'src/utils/returnNull';
 import { userAtom } from '../atoms/user';
 
 const Home = () => {
   const [user] = useAtom(userAtom);
-  const [tasks, setTasks] = useState<TaskModel[]>();
-  const fetchTasks = async () => {
-    const tasks = await apiClient.tasks.$get().catch(returnNull);
 
-    if (tasks !== null) setTasks(tasks);
-  };
-
-  useEffect(() => {
-    fetchTasks();
-  }, []);
-
-  if (!tasks || !user) return <Loading visible />;
+  if (!user) return <Loading visible />;
 
   return (
     <>
