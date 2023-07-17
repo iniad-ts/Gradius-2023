dd79758f19b9d1399144a32aefd83fa7fe838f72
1F10230183 <s1F102301831@iniad.org>
2023-07-17 13:53:16 +0900

    マージ分を修正

--- server/commonTypesWithClient/models.ts
+++ server/commonTypesWithClient/models.ts
@@ -1,4 +1,13 @@
-import type { TaskId, UserId } from './branded';
+import type {
+  EnemyId,
+  GameSessionId,
+  PlayerId,
+  PowerUpId,
+  SessionsEnemiesId,
+  StageId,
+  TaskId,
+  UserId,
+} from './branded';
 
 export type UserModel = {
   id: UserId;
@@ -12,4 +21,46 @@ export type TaskModel = {
   label: string;
   done: boolean;
   created: number;
+};
+
+export type GameSessionModel = {
+  id: GameSessionId;
+  playerId: PlayerId;
+  stageId: StageId;
+  score: number;
+  startTime: Date;
+  endTime?: Date | null;
+};
+
+export type PlayerModel = {
+  id: PlayerId;
+  name: string;
+  x: number;
+  y: number;
+};
+
+export type PlayerPowerUpModel = {
+  playerid: PlayerId;
+  powerUpId: PowerUpId;
+};
+
+export type PowerUpModel = {
+  id: PowerUpId;
+  name: string;
+};
+
+export type SessionsEnemiesModel = {
+  id: SessionsEnemiesId;
+  session_id: GameSessionId;
+  enemy_id: EnemyId;
+  x: number;
+  y: number;
+  hit_point: number;
+  collision_radius: number;
+};
+
+export type EnemyModel = {
+  id: EnemyId;
+  collision_radius: number;
+  score: number;
 };
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,26 +1,12 @@
-import type { TaskModel } from '$/commonTypesWithClient/models';
 import { useAtom } from 'jotai';
-import { useEffect, useState } from 'react';
 import { Loading } from 'src/components/Loading/Loading';
-import { apiClient } from 'src/utils/apiClient';
-import { returnNull } from 'src/utils/returnNull';
 import { userAtom } from '../atoms/user';
 import { App } from './test/test';
 
 const Home = () => {
   const [user] = useAtom(userAtom);
-  const [tasks, setTasks] = useState<TaskModel[]>();
-  const fetchTasks = async () => {
-    const tasks = await apiClient.tasks.$get().catch(returnNull);
 
-    if (tasks !== null) setTasks(tasks);
-  };
-
-  useEffect(() => {
-    fetchTasks();
-  }, []);
-
-  if (!tasks || !user) return <Loading visible />;
+  if (!user) return <Loading visible />;
 
   return (
     <>
