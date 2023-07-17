18f4752eaba3ec25365076fd5fe0e044e36c5618
1F10220146 <s1f102201469@iniad.org>
2023-07-17 19:35:43 +0900

    lint fixed

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,7 +1,6 @@
 import { useAtom } from 'jotai';
 import { Loading } from 'src/components/Loading/Loading';
 import { userAtom } from '../atoms/user';
-import { App } from './test/test';
 
 const Home = () => {
   const [user] = useAtom(userAtom);
