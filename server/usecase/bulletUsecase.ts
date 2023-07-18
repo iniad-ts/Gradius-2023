5eb022e6918760d90833f112d49ef130c5b2b9b3
Shiryu-Toujima-1f10210346 <s1f102103460@iniad.org>
2023-07-19 01:22:10 +0900

    bulletUsecase

--- server/usecase/bulletUsecase.ts
+++ server/usecase/bulletUsecase.ts
@@ -1,8 +1,10 @@
 export const bulletUsecase = {
   //xとyを保存するリスト
+  bulletCount: 0,
   bulletPosList: [] as { x: number; y: number }[],
   bulletPosSave: (x: number, y: number) => {
     bulletUsecase.bulletPosList.push({ x, y });
-    return true;
+    //list[n]を返す
+    return bulletUsecase.bulletPosList[bulletUsecase.bulletCount++];
   },
 };
