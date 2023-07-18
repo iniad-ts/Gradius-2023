f059965711b5502a1e1079bbb442cd52104ee275
s1f10230014 <s1f102300144@iniad.org>
2023-07-19 08:48:02 +0900

    server整理

--- server/repository/Usecase/laserUsecase.ts
+++ server/repository/Usecase/laserUsecase.ts
@@ -1,47 +1,77 @@
-//やることリスト
 import { enemy_list } from './enemyUsecase';
-import { player_now_position } from './playerUsecase';
+import { player_info } from './playerUsecase';
 
-//コードのレファクタリング
-export let laser_pos_list: number[][] = [];
+export type Laser_Info = {
+  pos: { x: number; y: number };
+  speed: number;
+  // direction: number;
+  radius: number;
+  // kind: number;
+  // userid: UserId;
+};
 
-// 発射した玉の位置を玉位置リストに追加
-export const make_laser = {
-  shot_laser: async () => {
-    laser_pos_list.push([player_now_position[0], player_now_position[1] - 10]);
+export const laseies_info: Laser_Info[] = [];
+
+//仮初期値
+const laser_speed = 10;
+const laser_radious = 10;
+
+//そのうち、弾の種類によって情報を変更できるようにしたい
+export const add_new_laser = {
+  shot_laser: () => {
+    const new_laser: Laser_Info = {
+      pos: { x: player_info.pos.x, y: player_info.pos.y - 10 },
+      speed: laser_speed,
+      radius: laser_radious,
+    };
+    laseies_info.push(new_laser);
   },
 };
 
 //敵の移動の関数に入れても良いかも
-
 setInterval(() => {
   move_laser();
   check_contact();
 }, 10);
 
-//玉進める、画面外消去
+// if (game_state === 'wait_start' && intervalId === null) {
+//   intervalId = setInterval(() => {
+//     move_laser();
+//     check_contact();
+//   }, 10);
+// }
+
+// if (game_state !== 'wait_start' && intervalId !== null) {
+//   clearInterval(intervalId);
+//   intervalId = null;
+// }
+
+//画面外消去処理も含む
+//フロント移植予定
 const move_laser = () => {
-  const new_laser_pos: number[][] = [];
-  for (const one_laser_pos of laser_pos_list) {
-    one_laser_pos[0] + 2 <= 1100 && new_laser_pos.push([one_laser_pos[0] + 10, one_laser_pos[1]]);
+  let i = 0;
+  for (const one_laser_info of laseies_info) {
+    one_laser_info.pos.x = one_laser_info.pos.x + 10;
+    one_laser_info.pos.x + 2 >= 1100 && laseies_info.splice(i, 1);
+    i++;
   }
-  laser_pos_list = new_laser_pos;
-  return laser_pos_list;
 };
 
-//当たり判定
+//mapで書き直す
+//フロント移植予定
 const check_contact = () => {
   let i = 0;
-  for (const one_laser_pos of laser_pos_list) {
+  for (const one_laser_info of laseies_info) {
     let h = 0;
     for (const one_enemy_pos of enemy_list) {
       if (
         Math.sqrt(
-          Math.pow(one_laser_pos[0] - one_enemy_pos[0], 2) +
-            Math.pow(one_laser_pos[1] - one_enemy_pos[1], 2)
-        ) <= 29
+          Math.pow(one_laser_info.pos.x - one_enemy_pos[0], 2) +
+            Math.pow(one_laser_info.pos.y - one_enemy_pos[1], 2)
+        ) <=
+        one_laser_info.radius + 20
       ) {
-        laser_pos_list.splice(i, 1);
+        laseies_info.splice(i, 1);
         enemy_list.splice(h, 1);
       }
       h++;
--- src/pages/gradius_game_screen/index.page.tsx
+++ src/pages/gradius_game_screen/index.page.tsx
@@ -1,4 +1,5 @@
 //ここにゲーム画面をつくる
+import type { Laser_Info } from '$/repository/Usecase/laserUsecase';
 import { useEffect, useState } from 'react';
 import { Layer, Rect, Stage, Wedge } from 'react-konva';
 import { Loading } from 'src/components/Loading/Loading';
@@ -6,16 +7,17 @@ import { apiClient } from 'src/utils/apiClient';
 const App = () => {
   const [fight_position, setfight_position] = useState<number[]>();
   const [enemies, setenemies] = useState<number[][]>([]);
-  const [laser_pos, setlaser_pos] = useState<number[][]>([]);
+  const [laseies_info, setlaseies_info] = useState<Laser_Info[]>([]);
+
   const fetchBord = async () => {
     const new_fighter_position = await apiClient.player.$get();
     const new_enemy_pos = await apiClient.enemy.$get();
-    const new_laser_pos = await apiClient.laser.$get();
+    const new_laseies_info = await apiClient.laser.$get();
     setfight_position(new_fighter_position);
     setenemies(new_enemy_pos);
-    setlaser_pos(new_laser_pos);
+    setlaseies_info(new_laseies_info);
   };
-  console.log(laser_pos);
+
   useEffect(() => {
     const cancellid = setInterval(fetchBord, 10);
     return () => {
@@ -46,15 +48,15 @@ const App = () => {
             y={enemy[1]}
           />
         ))}
-        {laser_pos.map((laser, index) => (
+        {laseies_info.map((laser, index) => (
           <Rect
             key={index}
             id={`laser_${index}`}
             fill="blue"
             width={20}
             height={20}
-            x={laser[0]}
-            y={laser[1]}
+            x={laser.pos.x}
+            y={laser.pos.y}
           />
         ))}
       </Layer>
