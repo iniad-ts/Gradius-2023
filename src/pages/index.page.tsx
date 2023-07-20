186ad9f36aaca745baee4a5dc6153dedd1adf320
s1f102003425 <s1f102003425@iniad.org>
2023-07-21 00:37:17 +0900

    zボタンを押すと弾が出る

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -21,7 +21,26 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
+  const [isFiring, setIsFiring] = useState(false);
+  const [shotX, setShotX] = useState(0);
+  const [shotY, setShotY] = useState(0);
+  const fireShot = () => {
+    if (!isFiring) {
+      setShotX(playerX);
+      setShotY(playerY);
+      setIsFiring(true);
+
+      const shotInterval = setInterval(() => {
+        setShotX((preX) => preX + 5);
+      }, 50);
+      setTimeout(() => {
+        clearInterval(shotInterval);
+        setIsFiring(false);
+      }, 300);
+    }
+  };
   const hoge = true;
+  // const [displayShot, setDisplayShot] = useState(false);
   const keydown = async (e: React.KeyboardEvent<HTMLDivElement>) => {
     console.log(e);
     console.log(e.code);
@@ -32,13 +51,21 @@ const Home = () => {
     setPlayerX(game.x);
     setPlayerY(game.y);
     setBoard(game.board);
+    if (e.code === 'KeyZ') {
+      fireShot();
+    }
     console.log(game.board);
   };
 
   const click = async (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     console.log(e);
   };
-  //ここまで
+  // const keyDownHandler = (e: React.KeyboardEvent<HTMLDivElement>) => {
+  //   const key = e.code;
+  //   if (key === 'keyZ') {
+  //     setShot(true);
+  //   }
+  // };
 
   if (!hoge) return <Loading visible />;
 
@@ -53,7 +80,7 @@ const Home = () => {
       >
         <div id="key">X:{playerX}</div>
         <div id="key">Y:{playerY}</div>
-        <div id="key" />
+        <div id="key">shot:{isFiring ? 'Fired' : 'Not fired'}</div>
       </div>
       <div
         id="player"
@@ -68,7 +95,6 @@ const Home = () => {
       />
       <div className={styles.board}>
         {board.map((row, y) =>
-          // eslint-disable-next-line complexity
           row.map((color, x) => (
             <div className={styles.cell} key={`${x}-${y}`} style={{ position: 'relative' }}>
               {color !== 0 && (
@@ -132,6 +158,20 @@ const Home = () => {
                   </Layer>
                 </Stage>
               )}
+              {isFiring && color !== 0 && (
+                <div
+                  id="shot"
+                  style={{
+                    position: 'absolute',
+                    left: `${shotX}px`,
+                    top: `${shotY + 8}px`,
+                    backgroundColor: 'blue',
+                    width: '20px',
+                    height: '10px',
+                    borderRadius: '50%',
+                  }}
+                />
+              )}
             </div>
           ))
         )}
@@ -140,14 +180,4 @@ const Home = () => {
   );
 };
 
-// Rectで書いたものをメモ用に残してます(すぐ消します)
-/*<Rect
-stroke="black"
-fill="white"
-strokeWidth={1}
-x={0}
-y={10}
-width={20}
-height={10}
-/>*/
 export default Home;
