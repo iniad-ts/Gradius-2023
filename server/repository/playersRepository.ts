35a971bd06cefa0606836ad64680ff27e1038cde
mst-mkt <s1f102303057@iniad.org>
2023-07-24 22:56:28 +0900

    データベース追加に伴うRepository等の追加

---
+++ server/repository/playersRepository.ts
@@ -0,0 +1,64 @@
+import type { UserId } from '$/commonTypesWithClient/branded';
+import type { PlayerModel } from '$/commonTypesWithClient/models';
+import { UserIdParser, gameIdParser } from '$/service/idParsers';
+import { prismaClient } from '$/service/prismaClient';
+import type { Player } from '@prisma/client';
+import { z } from 'zod';
+
+const toPlayerModel = (prismaPlayer: Player): PlayerModel => ({
+  id: UserIdParser.parse(prismaPlayer.id),
+  name: prismaPlayer.name,
+  createdAt: prismaPlayer.createdAt.getTime(),
+  updateAt: prismaPlayer.updatedAt.getTime(),
+  position: {
+    x: prismaPlayer.posX,
+    y: prismaPlayer.posY,
+  },
+  health: z.number().min(0).parse(prismaPlayer.health),
+  score: z.number().min(0).parse(prismaPlayer.score),
+  gameId: gameIdParser.parse(prismaPlayer.gameId),
+});
+
+export const playersRepository = {
+  getAll: async (): Promise<PlayerModel[] | null> => {
+    const prismaPlayers = await prismaClient.player.findMany();
+
+    return prismaPlayers.map(toPlayerModel);
+  },
+  getUnique: async (id: UserId): Promise<PlayerModel | null> => {
+    const prismaPlayer = await prismaClient.player.findUnique({
+      where: { id },
+    });
+
+    return prismaPlayer && toPlayerModel(prismaPlayer);
+  },
+  save: async (player: PlayerModel) => {
+    await prismaClient.player.upsert({
+      where: { id: player.id },
+      update: {
+        name: player.name,
+        posX: player.position.x,
+        posY: player.position.y,
+        health: player.health,
+        score: player.score,
+        updatedAt: new Date(player.updateAt),
+      },
+      create: {
+        id: player.id,
+        name: player.name,
+        posX: player.position.x,
+        posY: player.position.y,
+        health: player.health,
+        score: player.score,
+        createdAt: new Date(player.createdAt),
+        updatedAt: new Date(player.updateAt),
+        gameId: player.gameId,
+      },
+    });
+  },
+  delete: async (id: UserId) => {
+    await prismaClient.player.delete({
+      where: { id },
+    });
+  },
+};
