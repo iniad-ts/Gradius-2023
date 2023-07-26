ec822e10b3a2c7e2af07a31448d5125118b457c9
imoken777 <imoken89@gmail.com>
2023-07-26 18:51:17 +0900

    敵宇宙船画像表示と背景画像がスライドするように

--- src/pages/game/index.page.tsx
+++ src/pages/game/index.page.tsx
@@ -1,6 +1,6 @@
 import type { EnemyModel, PlayerModel } from '$/commonTypesWithClient/models';
 import type { BulletModelWithPosition } from '$/usecase/bulletUseCase';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Circle, Image, Layer, Stage } from 'react-konva';
 import { staticPath } from 'src/utils/$path';
 import { apiClient } from 'src/utils/apiClient';
@@ -11,6 +11,43 @@ const Game = () => {
   const [enemies, setEnemies] = useState<EnemyModel[]>([]);
   const [bullets, setBullets] = useState<BulletModelWithPosition[]>([]);
   const [shipImage] = useImage(staticPath.images.spaceship_png);
+  const [enemieShipImage] = useImage(staticPath.images.enemie_spaceship_png);
+
+  const BackgroundImage = () => {
+    const [image] = useImage(staticPath.images.space_background_png);
+    const [pos, setPos] = useState(0);
+    const imageWidth = image ? image.naturalWidth : 0;
+    const imageHeight = image ? image.naturalHeight : 0;
+    const aspectRatio = imageWidth / imageHeight;
+    const viewportHeight = window.innerHeight;
+    const viewportWidth = viewportHeight * aspectRatio;
+
+    useEffect(() => {
+      const animate = () => {
+        setPos((oldPos) => (oldPos - 2 <= -viewportWidth ? 0 : oldPos - 2));
+        requestAnimationFrame(animate);
+      };
+      animate();
+    }, [viewportWidth]);
+
+    return (
+      <>
+        <Image image={image} x={pos} width={viewportWidth} height={viewportHeight} />
+        <Image
+          image={image}
+          x={pos + viewportWidth}
+          width={viewportWidth}
+          height={viewportHeight}
+        />
+      </>
+    );
+  };
+
+  const backgroundImageRef = useRef<JSX.Element | null>(null);
+
+  if (!backgroundImageRef.current) {
+    backgroundImageRef.current = <BackgroundImage />;
+  }
 
   const fetchPlayers = async () => {
     const res = await apiClient.player.$get();
@@ -42,7 +79,8 @@ const Game = () => {
   });
   return (
     <div>
-      <Stage width={1000} height={700}>
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>{backgroundImageRef.current}</Layer>
         <Layer>
           {bullets.map((bullet) => (
             <Circle
@@ -69,11 +107,13 @@ const Game = () => {
         </Layer>
         <Layer>
           {enemies.map((enemy) => (
-            <Circle
-              fill="blue"
-              radius={enemy.radius}
-              x={enemy.position.x}
-              y={enemy.position.y}
+            <Image
+              image={enemieShipImage}
+              width={enemy.radius * 2}
+              height={enemy.radius * 2}
+              rotation={-90}
+              x={enemy.position.x + enemy.radius}
+              y={enemy.position.y - enemy.radius}
               key={enemy.id}
             />
           ))}
