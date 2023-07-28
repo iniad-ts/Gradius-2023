6879abb514d0578c34fb478b547fdaf701323ca4
enbipaipu <s1f102300122@iniad.org>
2023-07-28 23:01:25 +0900

    開発環境整理

---
+++ src/pages/controller/index.page.tsx
@@ -0,0 +1,17 @@
+import { useAtom } from 'jotai';
+import { userAtom } from 'src/atoms/user';
+import { Loading } from 'src/components/Loading/Loading';
+import styles from './controller.module.css';
+
+const Home = () => {
+  const [user] = useAtom(userAtom);
+  if (!user) return <Loading visible />;
+
+  return (
+    <>
+      <div className={styles.container}>{/*  */}</div>
+    </>
+  );
+};
+
+export default Home;
