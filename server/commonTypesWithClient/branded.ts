e860784de32840cf6f2861f59c7c72d46d7b9514
mst-mkt <s1f102303057@iniad.org>
2023-07-30 23:44:53 +0900

    add: 型定義ファイルを作成

--- server/commonTypesWithClient/branded.ts
+++ server/commonTypesWithClient/branded.ts
@@ -5,3 +5,9 @@ type Branded<T extends string> = string & z.BRAND<T>;
 export type UserId = Branded<'UserId'>;
 
 export type TaskId = Branded<'TaskId'>;
+
+export type GameId = Branded<'GameId'>;
+
+export type EnemyId = Branded<'EnemyId'>;
+
+export type BulletId = Branded<'BulletId'>;
