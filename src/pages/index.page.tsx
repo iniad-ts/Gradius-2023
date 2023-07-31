3f7164f5dcd04bf3c2ba8309cb11235f6861eadc
rarandeyo <s1F102301392@iniad.org>
2023-07-31 17:20:03 +0900

    基礎でぃうす

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -5,10 +5,12 @@ const Home = () => {
   const [gradius, setGradius] = useState([0, 0]);
   const [enemy, setEnemy] = useState([1000, 500]);
   const [gradiusBullet, setGradiusBullet] = useState([gradius[0] + 50, gradius[1] + 25]);
+  const windowWidth = Number(window.innerWidth);
+  const windowHeight = Number(window.innerHeight);
 
   const updateGradiusBullet = async () => {
     setGradiusBullet((prev) => {
-      if (prev[0] < 1280) {
+      if (prev[0] < windowWidth) {
         return [prev[0] + 10, prev[1]];
       } else {
         return prev;
@@ -26,9 +28,16 @@ const Home = () => {
 
   return (
     <>
-      <Stage width={1920} height={1080}>
+      <Stage width={windowWidth} height={windowHeight}>
         <Layer>
-          <Rect stroke="black" strokeWidth={1} x={0} y={0} width={1280} height={720} />
+          <Rect
+            stroke="black"
+            strokeWidth={1}
+            x={0}
+            y={0}
+            width={windowWidth}
+            height={windowHeight}
+          />
           <Rect x={gradius[0]} y={gradius[1]} width={50} height={50} fill="red" />
           <Rect x={enemy[0]} y={enemy[1]} width={50} height={50} fill="green" />
           <Circle x={gradiusBullet[0]} y={gradiusBullet[1]} radius={10} fill="blue" />
