2221904d448a36b23a5726a6f3bb3cdf95574f31
enbipaipu <s1f102300122@iniad.org>
2023-08-01 14:38:11 +0900

    はじく

--- server/usecase/playerUsecase.ts
+++ server/usecase/playerUsecase.ts
@@ -30,19 +30,19 @@ export const roomUsecase = (() => {
         gunShot();
       } else if (movedirection === 'up') {
         //飛行機が上に上がる
-        position[0] -= 50;
+        position[1] -= 50;
         result = 'UP が入力されました';
       } else if (movedirection === 'left') {
         //飛行機が左に移動
-        position[1] -= 10;
+        position[0] -= 10;
         result = 'LEFT が入力されました';
       } else if (movedirection === 'right') {
         //飛行機が下に下がる
-        position[1] += 10;
+        position[0] += 10;
         result = 'RIGHT が入力されました';
       } else {
         //飛行機が下に下がる
-        position[0] += 50;
+        position[1] += 50;
         result = 'DOWN が入力されました';
       }
       return result;
