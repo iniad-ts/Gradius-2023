21909f277ad6f518986cb548eb89c7d76a447621
1F10220146 <s1f102201469@iniad.org>
2023-08-05 05:40:10 +0900

    Usecaseのフォーマットを変更

--- server/usecase/enemyUsecase.ts
+++ server/usecase/enemyUsecase.ts
@@ -3,56 +3,44 @@ import { enemyRepository } from '$/repository/enemyRepository';
 import { EnemyIdParser } from '$/service/idParsers';
 import { randomUUID } from 'crypto';
 
-const create_enemy = async () => {
-  const new_enemy: EnemyModel = {
-    id: EnemyIdParser.parse(randomUUID()),
-    pos: { x: enemy_first_pos_x, y: Math.floor(Math.random() * 690) + 1 },
-    speed: enemy_speed,
-    hp: enemy_hp,
-    radius: enemy_radius,
-  };
-  await enemyRepository.save(new_enemy);
-};
-
-const move_Enemy = async () => {
-  const enemies: EnemyModel[] = await enemyRepository.getEnemies();
-  for (const enemy of enemies) {
-    const moved_enemy: EnemyModel = {
-      ...enemy,
-      //ここは、後でenemyの複雑な動きに対応させる
-      pos: { x: enemy.pos.x - 8, y: enemy.pos.y },
-    };
-    await enemyRepository.save(moved_enemy);
-  }
-};
-
-const delete_off_screen_enemy = async () => {
-  let enemies: EnemyModel[] = await enemyRepository.getEnemies();
-  enemies = enemies.filter((enemy) => {
-    //とりあえず50です
-    if (enemy.pos.x < 50) {
-      enemyRepository.declare(enemy.id);
-      return false;
-    } else {
-      return true;
-    }
-  });
-  //await Promise.allは、必要か微妙
-  //await Promise.all(enemies.map((enemy) => enemyRepository.save(enemy)));
-  enemies.map((enemy) => enemyRepository.save(enemy));
-};
-
 //敵の位置を取得する際にこれを使えば、全ての敵の情報が配列で返されます
 export const enemyUsecase = {
   //ここにあったgetEnemiesは、enemyRepositoryから呼び出す
   create_enemy: async () => {
-    await create_enemy();
+    const new_enemy: EnemyModel = {
+      id: EnemyIdParser.parse(randomUUID()),
+      pos: { x: enemy_first_pos_x, y: Math.floor(Math.random() * 690) + 1 },
+      speed: enemy_speed,
+      hp: enemy_hp,
+      radius: enemy_radius,
+    };
+    await enemyRepository.save(new_enemy);
   },
   move_Enemy: async () => {
-    await move_Enemy();
+    const enemies: EnemyModel[] = await enemyRepository.getEnemies();
+    for (const enemy of enemies) {
+      const moved_enemy: EnemyModel = {
+        ...enemy,
+        //ここは、後でenemyの複雑な動きに対応させる
+        pos: { x: enemy.pos.x - 8, y: enemy.pos.y },
+      };
+      await enemyRepository.save(moved_enemy);
+    }
   },
   delete_off_screen_enemy: async () => {
-    await delete_off_screen_enemy();
+    let enemies: EnemyModel[] = await enemyRepository.getEnemies();
+    enemies = enemies.filter((enemy) => {
+      //とりあえず50です
+      if (enemy.pos.x < 50) {
+        enemyRepository.declare(enemy.id);
+        return false;
+      } else {
+        return true;
+      }
+    });
+    //await Promise.allは、必要か微妙
+    //await Promise.all(enemies.map((enemy) => enemyRepository.save(enemy)));
+    enemies.map((enemy) => enemyRepository.save(enemy));
   },
 };
 
@@ -64,11 +52,11 @@ const enemy_hp = 10;
 
 setInterval(() => {
   // make_enemy();
-  create_enemy();
+  enemyUsecase.create_enemy();
 }, 9000);
 
 setInterval(() => {
   // move_or_delete_enemy();
-  move_Enemy();
-  delete_off_screen_enemy();
+  enemyUsecase.create_enemy();
+  enemyUsecase.create_enemy();
 }, 100);
