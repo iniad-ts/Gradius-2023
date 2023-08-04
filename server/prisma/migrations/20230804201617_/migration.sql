953e33301b2e44a343b62bc4cfd228b56fca7d31
1F10220146 <s1f102201469@iniad.org>
2023-08-05 05:16:29 +0900

    migrate

--- server/prisma/migrations/20230804201617_/migration.sql
+++ server/prisma/migrations/20230804201617_/migration.sql
@@ -1,4 +1,14 @@
 -- CreateTable
+CREATE TABLE "Task" (
+    "id" TEXT NOT NULL,
+    "label" TEXT NOT NULL,
+    "done" BOOLEAN NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Enemy" (
     "id" TEXT NOT NULL,
     "pos" JSONB NOT NULL,
