51bf42a5c6a683364bad093c4dc1760a00af84d3
s1f10230014 <s1f102300144@iniad.org>
2023-08-06 05:44:15 +0900

    修正

--- server/api/rooms/control/controller.ts
+++ server/api/rooms/control/controller.ts
@@ -2,17 +2,8 @@ import { playerUsecase } from '$/Usecase/playerUsecase';
 import { defineController } from './$relay';
 
 export default defineController(() => ({
-  get: async () => {
-    return {
-      status: 200,
-      body: await playerUsecase.getPlayerPos(),
-    };
-  },
-
-  post: async ({ body }) => {
-    return {
-      status: 200,
-      body: await playerUsecase.movePlayer(body),
-    };
-  },
+  get: async () => ({
+    status: 200,
+    body: await playerUsecase.getPlayerPos(),
+  }),
 }));
