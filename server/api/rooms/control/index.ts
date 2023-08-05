a161e65a26f6f210754cf396c36e4f4ac26f284f
1F10230183 <s1F102301831@iniad.org>
2023-08-06 06:40:17 +0900

    lintかけ忘れ

--- server/api/rooms/control/index.ts
+++ server/api/rooms/control/index.ts
@@ -1,10 +1,10 @@
-import type{ MoveDirection } from "$/Usecase/playerUsecase";
+import type { MoveDirection } from '$/Usecase/playerUsecase';
 
 export type Methods = {
   get: {
     resBody: number[][];
   };
-post:{
-  reqBody:MoveDirection
-}
+  post: {
+    reqBody: MoveDirection;
+  };
 };
