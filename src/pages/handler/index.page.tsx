13473cbf201913fb24d296c959c2c34a9c8b0dfa
s1f102003425 <s1f102003425@iniad.org>
2023-08-06 19:16:12 +0900

    十字キーでx,yの値をバックエンド経由で操作できるように

--- src/pages/handler/index.page.tsx
+++ src/pages/handler/index.page.tsx
@@ -1,23 +1,38 @@
-import { useAtom } from 'jotai';
+import { useState } from 'react';
 import { Loading } from 'src/components/Loading/Loading';
-import { BasicHeader } from 'src/pages/@components/BasicHeader/BasicHeader';
-import { userAtom } from '../../atoms/user';
-import styles from './index.module.css';
+import { apiClient } from 'src/utils/apiClient';
+// import styles from '../index.module.css';
 
 const Home = () => {
-  const [user] = useAtom(userAtom);
+  const hoge = true;
 
-  if (!user) return <Loading visible />;
+  const keydown = async (e: React.KeyboardEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    const game = await apiClient.handler.$post({
+      body: { position: layerPosition, key: e.code },
+    });
+    setLayerPosition(game.position);
+  };
+  // const click = async (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+  //   await apiClient.handler.$post();
+  //   console.log(e);
+  // };
+  const [layerPosition, setLayerPosition] = useState({ x: 0, y: 0 });
+  if (!hoge) return <Loading visible />;
+  console.log(`x:${layerPosition.x}y:${layerPosition.y}`);
 
   return (
     <>
-      <BasicHeader user={user} />
-      <div className={styles['handler-board']}>
+      <div className="debug-board" onKeyDown={keydown} tabIndex={0}>
+        <div id="key">X:{layerPosition.x}</div>
+        <div id="key">Y:{layerPosition.y}</div>
+      </div>
+      {/* <div className={styles['handler-board']} onKeyDown={keydown} tabIndex={0}>
         <button className={styles.handler}>-Y</button>
         <button className={styles.handler}>+Y</button>
         <button className={styles.handler}>-X</button>
         <button className={styles.handler}>+X</button>
-      </div>
+      </div> */}
     </>
   );
 };
