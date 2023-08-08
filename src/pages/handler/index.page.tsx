ce56b952fb7a19e8c0d5bed049503168b91a4e97
s1f102003425 <s1f102003425@iniad.org>
2023-08-08 22:47:18 +0900

    エラーの原因と思われる箇所をコメントアウトして一つ前のコミット内容に戻した

--- src/pages/handler/index.page.tsx
+++ src/pages/handler/index.page.tsx
@@ -6,34 +6,34 @@ import styles from './index.module.css';
 const Home = () => {
   const hoge = true;
 
-  // const keydown = async (e: React.KeyboardEvent<HTMLDivElement>) => {
-  //   e.preventDefault();
-  //   const game = await apiClient.handler.$post({
-  //     body: { position: layerPosition, key: e.code },
-  //   });
-  //   setLayerPosition(game.position);
-  // };
-  const handleButtonClick = async (operation: string) => {
+  const keydown = async (e: React.KeyboardEvent<HTMLDivElement>) => {
+    e.preventDefault();
     const game = await apiClient.handler.$post({
-      body: { position: layerPosition, key: operation },
+      body: { position: layerPosition, key: e.code },
     });
     setLayerPosition(game.position);
   };
-  // const click = async (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-  //   await apiClient.create.$post();
-  //   console.log(e);
+  // const handleButtonClick = async (operation: string) => {
+  //   const game = await apiClient.handler.$post({
+  //     body: { position: layerPosition, key: operation },
+  //   });
+  //   setLayerPosition(game.position);
   // };
+  const click = async (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    await apiClient.create.$post();
+    console.log(e);
+  };
   const [layerPosition, setLayerPosition] = useState({ x: 0, y: 0 });
   if (!hoge) return <Loading visible />;
   console.log(`x:${layerPosition.x}y:${layerPosition.y}`);
 
   return (
     <>
-      <div className={styles['debug-board']} tabIndex={0}>
+      <div className={styles['debug-board']} tabIndex={0} onKeyDown={keydown} onClick={click}>
         <div className={styles['debug-value']}>X:{layerPosition.x}</div>
         <div className={styles['debug-value']}>Y:{layerPosition.y}</div>
       </div>
-      <div className={styles['handler-board']} tabIndex={0}>
+      {/* <div className={styles['handler-board']}>
         <button className={styles['minus-y']} onClick={() => handleButtonClick('+Y')}>
           +Y
         </button>
@@ -46,7 +46,7 @@ const Home = () => {
         <button className={styles['plus-y']} onClick={() => handleButtonClick('-Y')}>
           -Y
         </button>
-      </div>
+      </div> */}
     </>
   );
 };
