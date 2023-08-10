5b24076e794c2cb5a8629d9ba1c5c8d326da6092
Yamsh39 <s1f102300043@iniad.org>
2023-08-11 01:25:26 +0900

    ジョイスティックとりあえず完成

--- src/pages/gradius_controller/index.page.tsx
+++ src/pages/gradius_controller/index.page.tsx
@@ -1,92 +1,50 @@
-import { useEffect, useRef, useState } from 'react';
-import { Loading } from 'src/components/Loading/Loading';
+import { useState } from 'react';
 import styles from './index.module.css';
 
 const Controller = () => {
-  const joystickLimitNumber = 35;
-  const joystickBallRef = useRef<HTMLDivElement | null>(null);
-  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [touchStartPosition, setTouchStartPosition] = useState({ x: 0, y: 0 });
+  const [stickPosition, setStickPosition] = useState({ x: 0, y: 0 });
+  const joystickLimit = 35;
 
-  const setupJoystick = () => {
-    if (joystickBallRef.current) {
-      const joystickBall = joystickBallRef.current;
-      const joystickCenterX =
-        joystickBall.getBoundingClientRect().left + joystickBall.clientWidth / 2;
-      const joystickCenterY =
-        joystickBall.getBoundingClientRect().top + joystickBall.clientHeight / 2;
-      return { joystickBall, joystickCenterX, joystickCenterY };
-    }
-    return null;
+  const handleTouchStart = (event: React.TouchEvent) => {
+    const { clientX, clientY } = event.touches[0];
+    setTouchStartPosition({ x: clientX, y: clientY });
   };
 
-  useEffect(() => {
-    const setupResult = setupJoystick();
-    if (setupResult) {
-      const { joystickBall, joystickCenterX, joystickCenterY } = setupResult;
+  const handleTouchMove = (event: React.TouchEvent) => {
+    const { clientX, clientY } = event.touches[0];
+    const deltaX = clientX - touchStartPosition.x;
+    const deltaY = clientY - touchStartPosition.y;
 
-      const handleTouchMove = (event: TouchEvent) => {
-        event.preventDefault();
-        const touch = event.touches[0];
-        const pageX = touch.pageX;
-        const pageY = touch.pageY;
+    const distance = Math.sqrt(deltaX ** 2 + deltaY ** 2);
 
-        const touchX =
-          Math.abs(pageX - joystickCenterX) < joystickLimitNumber
-            ? pageX - joystickCenterX
-            : pageX - joystickCenterX > 0
-            ? joystickLimitNumber
-            : -joystickLimitNumber;
-        const touchY =
-          Math.abs(pageY - joystickCenterY) < joystickLimitNumber
-            ? pageY - joystickCenterY
-            : pageY - joystickCenterY > 0
-            ? joystickLimitNumber
-            : -joystickLimitNumber;
-
-        joystickBall.style.left = `calc(50% + ${touchX}px)`;
-        joystickBall.style.top = `calc(50% + ${touchY}px)`;
-      };
-
-      const handleTouchEnd = () => {
-        joystickBall.style.top = '50%';
-        joystickBall.style.left = '50%';
-      };
-
-      joystickBall.addEventListener('touchmove', handleTouchMove);
-      joystickBall.addEventListener('touchend', handleTouchEnd);
-
-      return () => {
-        joystickBall.removeEventListener('touchmove', handleTouchMove);
-        joystickBall.removeEventListener('touchend', handleTouchEnd);
-      };
+    if (distance <= joystickLimit) {
+      setStickPosition({ x: deltaX, y: deltaY });
+    } else {
+      const angle = Math.atan2(deltaY, deltaX);
+      const constrainedX = joystickLimit * Math.cos(angle);
+      const constrainedY = joystickLimit * Math.sin(angle);
+      setStickPosition({ x: constrainedX, y: constrainedY });
     }
-  });
-
-  const getJoystickMove = (event: React.TouchEvent<HTMLDivElement>) => {
-    const touch = event.touches[0];
-    const x = touch.clientX;
-    const y = touch.clientY;
-    setPosition({ x, y });
   };
 
-  window.addEventListener('resize', () => {
-    const setupResult = setupJoystick();
-    if (setupResult && joystickBallRef.current) {
-      const { joystickCenterX, joystickCenterY } = setupResult;
-      joystickBallRef.current.style.left = `calc(50% + ${joystickCenterX}px)`;
-      joystickBallRef.current.style.top = `calc(50% + ${joystickCenterY}px)`;
-    }
-  });
+  const handleTouchEnd = () => {
+    setStickPosition({ x: 0, y: 0 });
+  };
 
-  if (!joystickBallRef.current) return <Loading visible />;
   return (
-    <div className={styles.joystick} id="joystick">
-      <div className={styles.joystickFrame} id="joystick-frame" onTouchMove={getJoystickMove}>
+    <div className={styles.base}>
+      <div
+        className={styles.joystick}
+        id="joystick"
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        onTouchEnd={handleTouchEnd}
+      >
         <div
-          className={styles.joystickBall}
+          className={styles.stick}
           id="joystick-ball"
-          ref={joystickBallRef}
-          style={{ left: position.x, top: position.y }}
+          style={{ transform: `translate(${stickPosition.x}px, ${stickPosition.y}px)` }}
         />
       </div>
     </div>
