30f476765d0bd17b04b454b898f7489bc19e3880
s1f10230014 <s1f102300144@iniad.org>
2023-08-12 15:55:58 +0900

    入力欄追加

--- src/components/Config/ConfigPage.tsx
+++ src/components/Config/ConfigPage.tsx
@@ -16,32 +16,46 @@ export const Config = () => {
             <tr>
               <td>基本playerspeed</td>
               <td>5</td>
-              <td> </td>
+              <td>
+                <input type="text" name="PlayerSpeed" placeholder="入力してください" />
+              </td>
             </tr>
             <tr>
               <td>基本playerサイズ</td>
-              <td>h:30,w:20</td>
-              <td />
+              <td>h:30 w:20</td>
+              <td>
+                <input type="text" name="PlayerSize_h" placeholder="高さを入力してください" />
+                <input type="text" name="PlayerSize_w" placeholder="幅を入力してください" />
+              </td>
             </tr>
             <tr>
               <td>敵の出現頻度</td>
               <td>2/秒</td>
-              <td />
+              <td>
+                <input type="text" name="Enemy" placeholder="入力してください" />
+              </td>
             </tr>
             <tr>
               <td>基本敵speed</td>
               <td>10</td>
-              <td />
+              <td>
+                <input type="text" name="EnemySpeed" placeholder="入力してください" />
+              </td>
             </tr>
             <tr>
               <td>基本敵サイズ</td>
               <td>h:30,w:20</td>
-              <td />
+              <td>
+                <input type="text" name="EnemySize_h" placeholder="高さを入力してください" />
+                <input type="text" name="EnemySize_w" placeholder="幅を入力してください" />
+              </td>
             </tr>
             <tr>
               <td>画面数</td>
               <td>15</td>
-              <td />
+              <td>
+                <input type="text" name="screen" placeholder="入力してください" />
+              </td>
             </tr>
           </tbody>
 
