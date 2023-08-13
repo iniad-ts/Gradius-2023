1cb83f634368891fae396c0a8cd183daec9cb374
1F10220146 <s1f102201469@iniad.org>
2023-08-14 01:34:55 +0900

    refactor:キャメルケースに統一

--- server/Usecase/enemyUsecase.ts
+++ server/Usecase/enemyUsecase.ts
@@ -9,10 +9,10 @@ import { position } from './playerUsecase';
 //敵の位置を取得する際にこれを使えば、全ての敵の情報が配列で返されます
 //repositoryからgetEnemiesを直接たたくとusecaseがなぜか更新されないため1時的な回避策としてusecaseをapiでたたいて認識させてます
 export const enemyUsecase = {
-  getAll_Enemies: async (): Promise<EnemyModel[]> => {
+  getAllEnemies: async (): Promise<EnemyModel[]> => {
     return await enemyRepository.getEnemies();
   },
-  delete_enemy: async (id: EnemyId) => {
+  deleteEnemy: async (id: EnemyId) => {
     try {
       await enemyRepository.declare(id);
     } catch (e) {
@@ -22,28 +22,28 @@ export const enemyUsecase = {
 };
 
 setInterval(() => {
-  create_enemy();
-  deleteOffScEreennemy();
+  createEnemy();
+  deleteOffScreenEnemy();
 }, 5000);
 
 setInterval(() => {
   // move_or_delete_enemy();
-  move_Enemy();
+  moveEnemy();
 }, 10);
 
 // 仮初期値
-const enemy_first_pos_x = 3000;
-const enemy_speed = 5;
-const enemy_radius = 20;
-const enemy_hp = 10;
+const enemyFirstPosX = 3000;
+const enemySpeed = 5;
+const enemyRadius = 20;
+const enemyHp = 10;
 
-const create_enemy = async () => {
+const createEnemy = async () => {
   const new_enemy: EnemyModel = {
     id: EnemyIdParser.parse(randomUUID()),
-    pos: { x: enemy_first_pos_x, y: Math.floor(Math.random() * 690) + 1 },
-    speed: enemy_speed,
-    hp: enemy_hp,
-    radius: enemy_radius,
+    pos: { x: enemyFirstPosX, y: Math.floor(Math.random() * 690) + 1 },
+    speed: enemySpeed,
+    hp: enemyHp,
+    radius: enemyRadius,
     type: 2,
     ///1-3のランダムな数値を返す
     /* type: Math.floor(Math.random() * 3) + 1, */
@@ -57,31 +57,31 @@ const create_enemy = async () => {
 }; */
 //y軸のみプレイヤーに追従する
 //delayは追従の遅れを表す
-const moveToplayer = (enemy: EnemyModel, player: number[][], delay: number) => {
+const moveToPlayer = (enemy: EnemyModel, player: number[][], delay: number) => {
   enemy.pos.x = enemy.pos.x - enemy.speed;
   enemy.pos.y = (player[0][1] + delay * enemy.pos.y) / (delay + 1);
   return { x: enemy.pos.x, y: enemy.pos.y };
 };
 
 //typeによって動きを変える
-const moveEnemyByplayer = (enemy: EnemyModel): { x: number; y: number } => {
+const moveEnemyByPlayer = (enemy: EnemyModel): { x: number; y: number } => {
   if (enemy.type === 1) {
     return { x: enemy.pos.x - enemy.speed, y: enemy.pos.y };
   } else if (enemy.type === 2) {
-    moveToplayer(enemy, position, 60);
+    moveToPlayer(enemy, position, 60);
     /* return { x: enemy.pos.x - 30, y: enemy.pos.y }; */
   } else if (enemy.type === 3) {
-    moveToplayer(enemy, position, 30);
+    moveToPlayer(enemy, position, 30);
     /*  return { x: enemy.pos.x - 100, y: enemy.pos.y }; */
   }
   return { x: enemy.pos.x, y: enemy.pos.y };
 };
 //敵を動かす
-const move_Enemy = async () => {
+const moveEnemy = async () => {
   const enemies: EnemyModel[] = await enemyRepository.getEnemies();
 
   for (const enemy of enemies) {
-    const newPos = moveEnemyByplayer(enemy);
+    const newPos = moveEnemyByPlayer(enemy);
     const moved_enemy: EnemyModel = {
       ...enemy,
       pos: newPos,
@@ -102,7 +102,7 @@ const deleteEnemy = async (id: EnemyId) => {
   }
 };
 //
-const deleteOffScEreennemy = async () => {
+const deleteOffScreenEnemy = async () => {
   const enemies: EnemyModel[] = await enemyRepository.getEnemies();
 
   const offScreenEnemiesIds = enemies.filter((enemy) => enemy.pos.x < 50).map((enemy) => enemy.id);
