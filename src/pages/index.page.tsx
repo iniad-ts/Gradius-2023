e9973aa79bbb626b884c6bf2555d693ae53a69b6
s1f10230014 <s1f102300144@iniad.org>
2023-08-14 07:47:47 +0900

    ユーザーがニックネームを入力するページを制作

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,71 +1,12 @@
-import { Rect } from 'react-konva';
+//この場所に書くのが正しいかは微妙
 
-const Home = () => {
-  const windowWidth = Number(window.innerWidth);
-  /*   const [playerPosition, setplayerPosition] = useState([0, 0]);
-  const [enemyPosition, setEnemyPosition] = useState([1000, 500]);
-  const [BulletPosition, setBulletPosition] = useState([
-    playerPosition[0] + 50,
-    playerPosition[1] + 25,
-  ]); */
-  // const [players, setPlayers] = useState([]);
-  // const [enemies, setEnemies] = useState([]);
-  // const [bullets, setBullets] = useState([]);
-  // const windowWidth = Number(window.innerWidth);
-  // const windowHeight = Number(window.innerHeight);
+import { InputYuserInfo } from 'src/components/InputYuserInfo/InputYuserInfo';
 
-  // const updateplayers = async () => {
-  //   const players = await apiClient.game.player.$get({
-  //     body: {},
-  //   });
-  // };
-
-  // const updatebullets = async () => {
-  //   const bullets = await apiClient.game.bullet.$get({
-  //     body: {},
-  //   });
-  // };
-
-  /*   const updateplayerBullet = async () => {
-    setBulletPosition((prev) => {
-      if (prev[0] < windowWidth) {
-        return [prev[0] + 10, prev[1]];
-      } else {
-        return prev;
-      }
-    });
-  }; */
-
-  // useEffect(() => {
-  //   const fetchplayers = setInterval(updateplayers, 50);
-  //   const fetchenemys = setInterval(updateenemies, 50);
-  //   const fetchbullets = setInterval(updatebullets, 50);
-  // });
-
+const InputInfo = () => {
   return (
-    /*     let player = players.map((player) => {
-    }) */
     <>
-      <Rect x={0} y={0} width={windowWidth} height={windowWidth} fill="black" />
-
-      {/*       <Stage width={windowWidth} height={windowHeight}>
-        <Layer>
-          <Rect
-            stroke="black"
-            strokeWidth={1}
-            x={0}
-            y={0}
-            width={windowWidth}
-            height={windowHeight}
-    
-      <Rect x={playerPosition[0]} y={playerPosition[1]} width={50} height={50} fill="red" />
-          <Rect x={enemyPosition[0]} y={enemyPosition[1]} width={50} height={50} fill="green" />
-          <Circle x={BulletPosition[0]} y={BulletPosition[1]} radius={10} fill="blue" />
-      </Layer>
-      </Stage>
-      ;  */}
+      <InputYuserInfo />
     </>
   );
 };
-
-export default Home;
+export default InputInfo;
