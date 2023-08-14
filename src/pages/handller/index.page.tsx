c3556b474f51eb1d04d2355abe44df86dd8ef655
1F10230250 <s1f102302506@iniad.org>
2023-08-14 19:41:04 +0900

    handller

--- src/pages/handller/index.page.tsx
+++ src/pages/handller/index.page.tsx
@@ -8,8 +8,8 @@ const Controller = () => {
   const [boxPosition, setBoxPosition] = useState('60%');
   const [circlePosition, setCirclePosition] = useState('15%');
 
-  const joystickRef = useRef(null);
-  const containerRef = useRef(null);
+  const joystickRef = useRef<HTMLDivElement | null>(null); // 指定した型で初期化
+  const containerRef = useRef<HTMLDivElement | null>(null); // 指定した型で初期化
   const isDragging = useRef(false);
   const startPosition = useRef({ x: 0, y: 0 });
 
@@ -18,9 +18,11 @@ const Controller = () => {
     setCirclePosition(circlePosition === '15%' ? '70%' : '15%');
   };
 
-  const handleJoystickTouchStart = (e) => {
+  const handleJoystickTouchStart = (e: React.TouchEvent) => {
+    // eの型を指定
     isDragging.current = true;
     const touch = e.targetTouches[0];
+    if (!containerRef.current) return; // nullチェック
     const containerRect = containerRef.current.getBoundingClientRect();
 
     const centerX = containerRect.left + containerRect.width / 2;
@@ -35,40 +37,60 @@ const Controller = () => {
     };
   };
 
-  const handleJoystickTouchMove = (e) => {
+  const handleJoystickTouchMove = (e: React.TouchEvent) => {
     if (!isDragging.current) return;
-    const touch = e.targetTouches[0];
-    const containerRect = containerRef.current.getBoundingClientRect();
 
+    const touch = e.targetTouches[0];
+    if (!containerRef.current || !joystickRef.current) return;
+
+    const containerRect = containerRef.current.getBoundingClientRect();
     const centerX = containerRect.left + containerRect.width / 2;
     const centerY = containerRect.top + containerRect.height / 2;
 
     const newJoystickX = touch.clientX - centerX - startPosition.current.x;
     const newJoystickY = touch.clientY - centerY - startPosition.current.y;
 
-    const maxDistance = containerRect.width / 2 - joystickRef.current.offsetWidth / 2 - 50;
+    const maxDistance = containerRect.width / 2 - (joystickRef.current.offsetWidth || 0) / 2 - 50;
     const distanceSquared = newJoystickX * newJoystickX + newJoystickY * newJoystickY;
 
-    if (distanceSquared <= maxDistance * maxDistance) {
-      const clampedX = newJoystickX;
-      const clampedY = newJoystickY;
+    const clampedValues = calculateClampedValues(
+      newJoystickX,
+      newJoystickY,
+      maxDistance,
+      distanceSquared
+    );
 
-      requestAnimationFrame(() => {
-        joystickRef.current.style.transform = `translate(${clampedX}px, ${clampedY}px) translate(-50%, -50%)`;
-      });
+    updateJoystickTransform(clampedValues.clampedX, clampedValues.clampedY);
+  };
+
+  const calculateClampedValues = (
+    newJoystickX: number,
+    newJoystickY: number,
+    maxDistance: number,
+    distanceSquared: number
+  ) => {
+    if (distanceSquared <= maxDistance * maxDistance) {
+      return { clampedX: newJoystickX, clampedY: newJoystickY };
     } else {
       const angle = Math.atan2(newJoystickY, newJoystickX);
       const clampedX = maxDistance * Math.cos(angle);
       const clampedY = maxDistance * Math.sin(angle);
-
-      requestAnimationFrame(() => {
-        joystickRef.current.style.transform = `translate(${clampedX}px, ${clampedY}px) translate(-50%, -50%)`;
-      });
+      return { clampedX, clampedY };
     }
   };
 
+  const updateJoystickTransform = (clampedX: number, clampedY: number) => {
+    requestAnimationFrame(() => {
+      if (joystickRef.current) {
+        joystickRef.current.style.transform = `translate(${clampedX}px, ${clampedY}px) translate(-50%, -50%)`;
+      }
+    });
+  };
+
   const handleJoystickTouchEnd = () => {
-    joystickRef.current.style.transform = 'translate(-50%, -50%)';
+    if (joystickRef.current) {
+      joystickRef.current.style.transform = 'translate(-50%, -50%)';
+    }
   };
 
   return (
