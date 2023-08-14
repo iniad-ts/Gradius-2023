6eef7aa724708a7551924c4afb91938a1dba840a
siko2525 <s1f102301820@iniad.org>
2023-08-14 14:26:43 +0900

    修正２

--- src/pages/gamescreen/index.page.tsx
+++ src/pages/gamescreen/index.page.tsx
@@ -10,28 +10,9 @@ const Home = () => {
   const [LeftArrow, setLeftArrow] = useState(false);
   const [RightArrow, setRightArrow] = useState(false);
   const [user] = useAtom(userAtom);
-  if (!user) return <Loading visible />;
-  return (
-    <Stage width={window.innerWidth} height={window.innerHeight}>
-      <Layer>
-        <Line
-          x={450}
-          y={200}
-          points={[0, 0, 100, 0, 50, 50]}
-          tension={0.2}
-          closed
-          stroke="black"
-          fillLinearGradientStartPoint={{ x: 80, y: 0 }}
-          fillLinearGradientEndPoint={{ x: 50, y: 50 }}
-          fillLinearGradientColorStops={[0, 'blue', 1, 'black']}
-        />
-        <Circle x={200} y={200} radius={30} fill="red" />
-      </Layer>
-    </Stage>
-  );
 
   useEffect(() => {
-    const keydown = (event: KeyboardEvent) => {
+    const keyDown = (event: KeyboardEvent) => {
       if (event.key === 'ArrowUp') {
         setUpArrow(true);
       } else if (event.key === 'ArrowDown') {
@@ -63,5 +44,25 @@ const Home = () => {
       window.removeEventListener('keyup', keyUp);
     };
   }, []);
+
+  if (!user) return <Loading visible />;
+  return (
+    <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Layer>
+        <Line
+          x={450}
+          y={200}
+          points={[0, 0, 100, 0, 50, 50]}
+          tension={0.2}
+          closed
+          stroke="black"
+          fillLinearGradientStartPoint={{ x: 80, y: 0 }}
+          fillLinearGradientEndPoint={{ x: 50, y: 50 }}
+          fillLinearGradientColorStops={[0, 'blue', 1, 'black']}
+        />
+        <Circle x={200} y={200} radius={30} fill="red" />
+      </Layer>
+    </Stage>
+  );
 };
 export default Home;
