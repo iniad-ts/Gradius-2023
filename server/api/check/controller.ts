8bb4cc6112f8aeefa934b0d3f7650978ce58d372
1F10220146 <s1f102201469@iniad.org>
2023-08-17 17:54:33 +0900

    Revert "api修正"
    This reverts commit 142a3b2c8b0fa81bbfaf042b84e478b78497347f.

--- server/api/check/controller.ts
+++ server/api/check/controller.ts
@@ -1,6 +1,5 @@
-import { enemyUsecase } from '$/usecase/enemyUsecase';
 import { defineController } from './$relay';
 
 export default defineController(() => ({
-  get: async () => ({ status: 200, body: await enemyUsecase.getEnemies() }),
+  get: () => ({ status: 200, body: 'Hello' }),
 }));
