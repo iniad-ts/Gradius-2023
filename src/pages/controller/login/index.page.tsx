24a212dfd90577ea090730d69fca5ec4ff995781
mst-mkt <s1f102303057@iniad.org>
2023-08-17 16:45:48 +0900

    Revert "ログイン画面のデザインを多少まともに"
    This reverts commit 506a46e5873cfd880b5a2ac32c4d62670ccd0a55.

--- src/pages/controller/login/index.page.tsx
+++ src/pages/controller/login/index.page.tsx
@@ -27,17 +27,12 @@ const Login = () => {
   };
 
   return (
-    <div className={styles.all}>
-      <h1 className={styles.text}>INIAD.tsにようこそ</h1>
-      <h2 className={styles.text2}>ゲームで使用する名前を入力してください</h2>
-      <div className={styles.box}>
-        <input type="text" value={name} onChange={handleChange} className={styles.input} />
-      </div>
-      <div className={styles.box}>
-        <button onClick={login} disabled={isButtonDisabled} className={styles.button}>
-          決定
-        </button>
-      </div>
+    <div>
+      <h1>Login</h1>
+      <input type="text" value={name} onChange={handleChange} />
+      <button onClick={login} disabled={isButtonDisabled}>
+        ログイン
+      </button>
     </div>
   );
 };
