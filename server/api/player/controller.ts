d6388c5ff9bd495dda913e3e5449c07aa894d2ce
mst-mkt <s1f102303057@iniad.org>
2023-08-17 17:16:26 +0900

    Revert "Merge pull request #5 from mst-mkt/mst-mkt"
    This reverts commit aa5a31db7521d770391c440af60c1bb0ef0b6db1, reversing
    changes made to a96fbe28972ef20694d67cc5f04bd9cf2f7abec0.

--- server/api/player/controller.ts
+++
@@ -1,7 +0,0 @@
-import { controlUseCase } from '$/usecase/controlUseCase';
-import { defineController } from './$relay';
-
-export default defineController(() => ({
-  get: () => ({ status: 200, body: controlUseCase.getPosition }),
-  post: ({ body }) => ({ status: 200, body: controlUseCase.move(body) }),
-}));
