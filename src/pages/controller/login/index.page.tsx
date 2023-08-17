19bc50c6fb69ade73655670ef585cceec0e77969
mst-mkt <s1f102303057@iniad.org>
2023-08-17 16:45:03 +0900

    Revert "指摘を修正"
    This reverts commit 86477f094b2e903e2d4c6249702da1a2bf478f4a.

--- src/pages/controller/login/index.page.tsx
+++ src/pages/controller/login/index.page.tsx
@@ -27,12 +27,17 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <h1>Login</h1>
-      <input type="text" value={name} onChange={handleChange} />
-      <button onClick={login} disabled={isButtonDisabled}>
-        ログイン
-      </button>
+    <div className={styles.all}>
+      <h1 className={styles.text}>INIAD.tsにようこそ</h1>
+      <h2 className={styles.text2}>ゲームで使用する名前を入力してください</h2>
+      <div className={styles.box}>
+        <input type="text" value={name} onChange={handleChange} className={styles.input} />
+      </div>
+      <div className={styles.box}>
+        <button onClick={login} disabled={isButtonDisabled} className={styles.button}>
+          決定
+        </button>
+      </div>
     </div>
   );
 };
