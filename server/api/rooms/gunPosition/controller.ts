c70e9d8861bced50b35033ef1c0c6522d20eb290
1F10220146 <s1f102201469@iniad.org>
2023-08-17 16:51:33 +0900

    Revert "errer解消"
    This reverts commit 7adf89693af0a850dd42f45aeae1347e052ed84c.

--- server/api/rooms/gunPosition/controller.ts
+++ server/api/rooms/gunPosition/controller.ts
@@ -1,9 +1,11 @@
 import { bulletUsecase } from '$/Usecase/bulletUsecase';
+import { bulletPosition, gunShot } from '$/Usecase/playerUsecase';
 import { defineController } from './$relay';
 
 export default defineController(() => ({
   get: async () => ({ status: 200, body: await bulletUsecase.getBullets() }),
   post: async ({ body }) => {
-    return { status: 200, body: await bulletUsecase.createBullet(body) };
+    await gunShot(body);
+    return { status: 200, body: bulletPosition };
   },
 }));
