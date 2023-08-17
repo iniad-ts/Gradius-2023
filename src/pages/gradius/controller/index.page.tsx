7eb4f5d47072c576da980c209ccf506d9807e400
1f10230234 <s1f102302348@iniad.org>
2023-08-17 14:57:05 +0900

    Revert "ボタン作った"
    This reverts commit 3a1f3314cf838e753cb65634acde1dfafa0a5d86.

--- src/pages/gradius/controller/index.page.tsx
+++ src/pages/gradius/controller/index.page.tsx
@@ -1,37 +1,10 @@
 //reactを使う->import
-import { apiClient } from 'src/utils/apiClient';
-import styles from './index.module.css';
-//////////////////上下左右前後に応じてpostするときのresBodyの値を0-5で変える
-//////////////////上:4 下:1 左:0 右:3 前:2 後:5にしてほしい
-const buttomMap = [
-  [9, 4, 9],
-  [0, 9, 3],
-  [9, 1, 9],
-];
 //home関数
-const Home = () => {
-  // 各方向にpostする関数
-  const postDirection = async (direction: number) => {
-    await apiClient.gradius.game.post({ body: direction });
-  };
-
-  //////////////////多分前後はつかわないけど作っといて
-  //////////////////発射はまた別のpostを用意するからいったんいいや
-  return (
-    <div className={styles.container}>
-      <div className={styles.wrapper}>
-        {buttomMap.map((row, y) =>
-          row.map((val, x) => (
-            <div
-              className={styles.buttom}
-              style={{ backgroundColor: val === 9 ? '#0000' : '#f80' }}
-              onClick={() => postDirection(val)}
-              key={`${y}${x}`}
-            />
-          ))
-        )}
-      </div>
-    </div>
-  );
-};
-export default Home;
+//押された用の関数->クリックされたときにpostする。書き方はsrc/pages/index.page.tsxのtaskのpostのしかたとかをみてがんばる
+//////////////////上下左右前後に応じてpostするときのresBodyの値を0-5で変える
+//////////////////上:1 下:4 左:0 右:3 前:2 後:5にしてほしい
+//////////////////多分前後はつかわないけど作っといて
+//////////////////発射はまた別のpostを用意するからいったんいいや
+//return(
+//// 上下左右＋発射のボタンを作る
+//)
