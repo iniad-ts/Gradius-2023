0d02bb1c293e3ce21362daf22515459c92f12685
1F10220146 <s1f102201469@iniad.org>
2023-08-17 15:35:28 +0900

    Revert "lint fixed"
    This reverts commit 18f4752eaba3ec25365076fd5fe0e044e36c5618.

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -2,6 +2,7 @@ import { useAtom } from 'jotai';
 import { Loading } from 'src/components/Loading/Loading';
 import { App } from 'src/test/test';
 import { userAtom } from '../atoms/user';
+import { App } from './test/test';
 
 const Home = () => {
   const [user] = useAtom(userAtom);
