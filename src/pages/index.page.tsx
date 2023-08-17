573a3b736169a16bee31c5a5a7b967edd1a06a2b
1F10220146 <s1f102201469@iniad.org>
2023-08-17 18:11:44 +0900

    Revert "いい加減にして２"
    This reverts commit 7fa06a0f18801d389b886a5ee00e56fcd38a2729, reversing
    changes made to 540c51bd0a86638f4654a06610e9de360c83d0eb.

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -2,40 +2,22 @@ import type { TaskModel } from '$/commonTypesWithClient/models';
 import { useAtom } from 'jotai';
 import type { ChangeEvent, FormEvent } from 'react';
 import { useEffect, useState } from 'react';
-import { Circle, Layer, Rect, Stage } from 'react-konva';
+import { Loading } from 'src/components/Loading/Loading';
+import { BasicHeader } from 'src/pages/@components/BasicHeader/BasicHeader';
 import { apiClient } from 'src/utils/apiClient';
+import { returnNull } from 'src/utils/returnNull';
+import { userAtom } from '../atoms/user';
+import styles from './index.module.css';
 
 const Home = () => {
-  /*   const [playerPosition, setplayerPosition] = useState([0, 0]);
-  const [enemyPosition, setEnemyPosition] = useState([1000, 500]);
-  const [BulletPosition, setBulletPosition] = useState([
-    playerPosition[0] + 50,
-    playerPosition[1] + 25,
-  ]); */
-  const [players, setPlayers] = useState([]);
-  const [enemies, setEnemies] = useState([]);
-  const [bullets, setBullets] = useState([]);
+  const [gradius, setGradius] = useState([0, 0]);
+  const [enemy, setEnemy] = useState([1000, 500]);
+  const [gradiusBullet, setGradiusBullet] = useState([gradius[0] + 50, gradius[1] + 25]);
   const windowWidth = Number(window.innerWidth);
   const windowHeight = Number(window.innerHeight);
 
-  const updateplayers = async () => {
-    const players = await apiClient.game.player.$get({
-      body: {},
-    });
-  };
-  const updateenemies = async () => {
-    const enemies = await apiClient.game.enemy.$get({
-      body: {},
-    });
-  };
-  const updatebullets = async () => {
-    const bullets = await apiClient.game.bullet.$get({
-      body: {},
-    });
-  };
-
-  /*   const updateplayerBullet = async () => {
-    setBulletPosition((prev) => {
+  const updateGradiusBullet = async () => {
+    setGradiusBullet((prev) => {
       if (prev[0] < windowWidth) {
         return [prev[0] + 10, prev[1]];
       } else {
@@ -48,10 +30,9 @@ const Home = () => {
     fetchTasks();
   }, []);
 
-  useEffect(() => {
-    const fetchplayers = setInterval(updateplayers, 50);
-    const fetchenemys = setInterval(updateenemies, 50);
-    const fetchbullets = setInterval(updatebullets, 50);
+    return () => {
+      clearInterval(timer);
+    };
   });
 
   return (
@@ -66,9 +47,9 @@ const Home = () => {
             width={windowWidth}
             height={windowHeight}
           />
-          <Rect x={playerPosition[0]} y={playerPosition[1]} width={50} height={50} fill="red" />
-          <Rect x={enemyPosition[0]} y={enemyPosition[1]} width={50} height={50} fill="green" />
-          <Circle x={BulletPosition[0]} y={BulletPosition[1]} radius={10} fill="blue" />
+          <Rect x={gradius[0]} y={gradius[1]} width={50} height={50} fill="red" />
+          <Rect x={enemy[0]} y={enemy[1]} width={50} height={50} fill="green" />
+          <Circle x={gradiusBullet[0]} y={gradiusBullet[1]} radius={10} fill="blue" />
         </Layer>
       </Stage>
       ;
