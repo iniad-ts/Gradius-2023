6870122e754155ad8a304efbe269b320cf2c6e7b
1F10220146 <s1f102201469@iniad.org>
2023-08-17 15:38:49 +0900

    Revert "Merge pull request #15 from g-Ratie/g-Ratie"
    This reverts commit 7cfd511b1870fb4ff4d2c41a953f01c4cd53b19d, reversing
    changes made to 6376c2effbdc87336cc74260829c61bff7ecc737.

--- server/commonTypesWithClient/models.ts
+++ server/commonTypesWithClient/models.ts
@@ -13,3 +13,45 @@ export type TaskModel = {
   done: boolean;
   created: number;
 };
+
+export type GameSessionModel = {
+  id: GameSessionId;
+  playerId: PlayerId;
+  stageId: StageId;
+  score: number;
+  startTime: Date;
+  endTime?: Date | null;
+};
+
+export type PlayerModel = {
+  id: PlayerId;
+  name: string;
+  x: number;
+  y: number;
+};
+
+export type PlayerPowerUpModel = {
+  playerid: PlayerId;
+  powerUpId: PowerUpId;
+};
+
+export type PowerUpModel = {
+  id: PowerUpId;
+  name: string;
+};
+
+export type SessionsEnemiesModel = {
+  id: SessionsEnemiesId;
+  session_id: GameSessionId;
+  enemy_id: EnemyId;
+  x: number;
+  y: number;
+  hit_point: number;
+  collision_radius: number;
+};
+
+export type EnemyModel = {
+  id: EnemyId;
+  collision_radius: number;
+  score: number;
+};
