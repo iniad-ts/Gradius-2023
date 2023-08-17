8674b662eab794c18b6a88fb1e821c3e138ce4f0
1F10220146 <s1f102201469@iniad.org>
2023-08-17 18:25:18 +0900

    Revert "基礎でぃうす"
    This reverts commit 3f7164f5dcd04bf3c2ba8309cb11235f6861eadc.

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -24,6 +24,9 @@ const Home = () => {
   const createTask = async (e: FormEvent) => {
     e.preventDefault();
     if (!label) return;
+  const [gradius, setGradius] = useState([0, 0]);
+  const [enemy, setEnemy] = useState([1000, 500]);
+  const [gradiusBullet, setGradiusBullet] = useState([gradius[0] + 50, gradius[1] + 25]);
 
     await apiClient.tasks.post({ body: { label } });
     setLabel('');
@@ -36,6 +39,14 @@ const Home = () => {
   const deleteTask = async (task: TaskModel) => {
     await apiClient.tasks._taskId(task.id).delete();
     await fetchTasks();
+  const updateGradiusBullet = async () => {
+    setGradiusBullet((prev) => {
+      if (prev[0] < 1280) {
+        return [prev[0] + 10, prev[1]];
+      } else {
+        return prev;
+      }
+    });
   };
 
   useEffect(() => {
@@ -71,6 +82,15 @@ const Home = () => {
           </li>
         ))}
       </ul>
+      <Stage width={1920} height={1080}>
+        <Layer>
+          <Rect stroke="black" strokeWidth={1} x={0} y={0} width={1280} height={720} />
+          <Rect x={gradius[0]} y={gradius[1]} width={50} height={50} fill="red" />
+          <Rect x={enemy[0]} y={enemy[1]} width={50} height={50} fill="green" />
+          <Circle x={gradiusBullet[0]} y={gradiusBullet[1]} radius={10} fill="blue" />
+        </Layer>
+      </Stage>
+      ;
     </>
   );
 };
