bbcb49bb900c195cfc3e638015ac1c2a65a48738
1F10220146 <s1f102201469@iniad.org>
2023-08-17 18:11:44 +0900

    Revert "Merge branch 'main' into main"
    This reverts commit 4e28158e7e45cbbe6e1e0403debb3d9ac7f32a08, reversing
    changes made to 540c51bd0a86638f4654a06610e9de360c83d0eb.

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -10,49 +10,67 @@ import { userAtom } from '../atoms/user';
 import styles from './index.module.css';
 
 const Home = () => {
-  const [gradius, setGradius] = useState([0, 0]);
-  const [enemy, setEnemy] = useState([1000, 500]);
-  const [gradiusBullet, setGradiusBullet] = useState([gradius[0] + 50, gradius[1] + 25]);
-  const windowWidth = Number(window.innerWidth);
-  const windowHeight = Number(window.innerHeight);
+  const [user] = useAtom(userAtom);
+  const [tasks, setTasks] = useState<TaskModel[]>();
+  const [label, setLabel] = useState('');
+  const inputLabel = (e: ChangeEvent<HTMLInputElement>) => {
+    setLabel(e.target.value);
+  };
+  const fetchTasks = async () => {
+    const tasks = await apiClient.tasks.$get().catch(returnNull);
 
-  const updateGradiusBullet = async () => {
-    setGradiusBullet((prev) => {
-      if (prev[0] < windowWidth) {
-        return [prev[0] + 10, prev[1]];
-      } else {
-        return prev;
-      }
-    });
+    if (tasks !== null) setTasks(tasks);
+  };
+  const createTask = async (e: FormEvent) => {
+    e.preventDefault();
+    if (!label) return;
+
+    await apiClient.tasks.post({ body: { label } });
+    setLabel('');
+    await fetchTasks();
+  };
+  const toggleDone = async (task: TaskModel) => {
+    await apiClient.tasks._taskId(task.id).patch({ body: { done: !task.done } });
+    await fetchTasks();
+  };
+  const deleteTask = async (task: TaskModel) => {
+    await apiClient.tasks._taskId(task.id).delete();
+    await fetchTasks();
   };
 
   useEffect(() => {
     fetchTasks();
   }, []);
 
-    return () => {
-      clearInterval(timer);
-    };
-  });
+  if (!tasks || !user) return <Loading visible />;
 
   return (
     <>
-      <Stage width={windowWidth} height={windowHeight}>
-        <Layer>
-          <Rect
-            stroke="black"
-            strokeWidth={1}
-            x={0}
-            y={0}
-            width={windowWidth}
-            height={windowHeight}
-          />
-          <Rect x={gradius[0]} y={gradius[1]} width={50} height={50} fill="red" />
-          <Rect x={enemy[0]} y={enemy[1]} width={50} height={50} fill="green" />
-          <Circle x={gradiusBullet[0]} y={gradiusBullet[1]} radius={10} fill="blue" />
-        </Layer>
-      </Stage>
-      ;
+      <BasicHeader user={user} />
+      <div className={styles.title} style={{ marginTop: '160px' }}>
+        Welcome to frourio!
+      </div>
+
+      <form style={{ textAlign: 'center', marginTop: '80px' }} onSubmit={createTask}>
+        <input value={label} type="text" onChange={inputLabel} />
+        <input type="submit" value="ADD" />
+      </form>
+      <ul className={styles.tasks}>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <label>
+              <input type="checkbox" checked={task.done} onChange={() => toggleDone(task)} />
+              <span>{task.label}</span>
+            </label>
+            <input
+              type="button"
+              value="DELETE"
+              className={styles.deleteBtn}
+              onClick={() => deleteTask(task)}
+            />
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
