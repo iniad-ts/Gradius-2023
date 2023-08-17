a8046944b4b45361a92df3ea4263064ceaf48e1c
1F10220146 <s1f102201469@iniad.org>
2023-08-17 15:44:07 +0900

    Revert "Merge pull request #7 from g-Ratie/g-Ratie"
    This reverts commit 257cf1ebeb9afa8a6ee3cc9f90342f62312788de, reversing
    changes made to d23f063a4245004447fa9f67093a3008f2274aa9.

--- server/api/player/shoot/index.ts
+++
@@ -1,9 +0,0 @@
-import type { Bullet } from '$/Usecase/bulletUsecase';
-import type { Player } from '$/Usecase/playerUsecase';
-
-export type Methods = {
-  post: {
-    reqBody: Player;
-    resBody: Bullet;
-  };
-};
