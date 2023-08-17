57d81458a7bc1a1c9ed3deb370a3fddbaa606a63
mst-mkt <s1f102303057@iniad.org>
2023-08-17 17:16:17 +0900

    Revert "Merge branch 'main' of github.com:mst-mkt/gradius into mst-mkt"
    This reverts commit db88b430e3ebcb2f882ec6733c878e69a13c392f, reversing
    changes made to c0b898ed4c5eef5172758e016eb36db37e561cb7.

--- src/pages/game/index.page.tsx
+++ src/pages/game/index.page.tsx
@@ -1,29 +1,18 @@
-import type { Bullet } from '$/usecase/bulletUseCase';
 import { useEffect, useState } from 'react';
-import { Circle, Layer, Rect, Stage, Wedge } from 'react-konva';
+import { Layer, Stage, Wedge } from 'react-konva';
 import { apiClient } from 'src/utils/apiClient';
 
 const Game = () => {
-  const [myPosition, setmyPosition] = useState({ x: 100, y: 350 });
-  const [enemyPosition, setenemyPosition] = useState({ x: 900, y: 350 });
-  const [bulletPosition, setbulletPosition] = useState<Bullet[]>([]);
+  const [position, setPosition] = useState({ x: 100, y: 350 });
   const fetchPosition = async () => {
     const res = await apiClient.player.$get();
     if (res) {
-      setmyPosition(res);
-    }
-  };
-  const fetchbulletPosition = async () => {
-    const res = await apiClient.player.bullet.$get();
-    if (res) {
-      setbulletPosition(res);
+      setPosition(res);
     }
   };
   useEffect(() => {
     const cancelId = setInterval(() => {
-      console.log('fetch');
       fetchPosition();
-      fetchbulletPosition();
     }, 100);
     return () => clearInterval(cancelId);
   });
@@ -32,31 +21,7 @@ const Game = () => {
     <div>
       <Stage width={1000} height={700}>
         <Layer>
-          <Wedge
-            fill="red"
-            angle={60}
-            radius={70}
-            rotation={150}
-            x={myPosition.x}
-            y={myPosition.y}
-          />
-        </Layer>
-        <Layer>
-          {bulletPosition.map((bullet) => (
-            <Circle
-              fill="red"
-              radius={bullet.radius}
-              x={bullet.position.x}
-              y={bullet.position.y}
-              key={bullet.position.x + bullet.position.y}
-            />
-          ))}
-        </Layer>
-        <Layer>
-          <Rect fill="blue" x={enemyPosition.x} y={enemyPosition.y} width={40} height={40} />
-        </Layer>
-        <Layer>
-          <Circle fill="blue" radius={7} x={enemyPosition.x - 100} y={enemyPosition.y} />
+          <Wedge angle={60} fill="red" radius={70} rotation={150} x={position.x} y={position.y} />
         </Layer>
       </Stage>
     </div>
