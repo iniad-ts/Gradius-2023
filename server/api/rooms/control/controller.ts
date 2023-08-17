ab57bb3e5a912f83089fe6331d567f08a1ae5a8d
1F10220146 <s1f102201469@iniad.org>
2023-08-17 16:54:08 +0900

    Revert "testエラー修正"
    This reverts commit 39fdacd0e5cae6cd3e9273973114579acc3f3c75.

--- server/api/rooms/control/controller.ts
+++ server/api/rooms/control/controller.ts
@@ -4,7 +4,7 @@ import { defineController } from './$relay';
 export default defineController(() => ({
   get: async () => ({
     status: 200,
-    body: await playerUsecase.getPlayerPos(),
+    body: await playerUsecase.getPlayers(),
   }),
   post: async ({ body }) => ({
     status: 200,
