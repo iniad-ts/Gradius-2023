af75670c65ab0dc5ecd38c6e03401be925a68c09
mst-mkt <s1f102303057@iniad.org>
2023-08-17 16:51:57 +0900

    Revert "&&->if"
    This reverts commit 386fc7c2721e5cf87a10d07d17c5f93d45f1a3a3.

---
+++ server/useCase/enemiesUseCase.ts
@@ -0,0 +1,30 @@
+import type { UserId } from '$/commonTypesWithClient/branded';
+import type { EnemyModel } from '$/commonTypesWithClient/models';
+import { enemiesRepository } from '$/repository/enemiesRepository';
+import { playersRepository } from '$/repository/playersRepository';
+import { enemyIdParser } from '$/service/idParsers';
+import { randomUUID } from 'crypto';
+import { playerUseCase } from './playerUseCase';
+
+export const enemyUseCase = {
+  create: async (): Promise<EnemyModel | null> => {
+    const newEnemy: EnemyModel = {
+      id: enemyIdParser.parse(randomUUID()),
+      createdPosition: {
+        x: Math.floor(Math.random() * 1920),
+        y: Math.floor(Math.random() * 1080),
+      },
+      createdAt: Date.now(),
+      type: 0,
+    };
+    console.log('create');
+    await enemiesRepository.create(newEnemy);
+    return newEnemy;
+  },
+  delete: async (enemyId: string, userId: UserId) => {
+    await enemiesRepository.delete(enemyId);
+    const userStatus = await playerUseCase.getStatus(userId, null);
+    userStatus !== null &&
+      (await playersRepository.save({ ...userStatus, score: userStatus.score + 1 }));
+  },
+};
