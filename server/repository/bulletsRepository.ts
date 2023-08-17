5041193a5f0c93f2b0f85272d3df84d2fd4d6f1e
mst-mkt <s1f102303057@iniad.org>
2023-08-17 17:15:03 +0900

    Revert "怒涛のエラー修正 vol.2"
    This reverts commit 4a0c3fd0f7815982a70ea603428268bad5dc0873.

--- server/repository/bulletsRepository.ts
+++ server/repository/bulletsRepository.ts
@@ -24,9 +24,7 @@ const toBulletModel = (prismaBullet: Bullet): BulletModel => ({
 export const bulletsRepository = {
   getAll: async (): Promise<BulletModel[] | null> => {
     const prismaBullets = await prismaClient.bullet.findMany({
-      where: {
-        exists: true,
-      },
+      where: { exists: true },
     });
 
     return prismaBullets.map(toBulletModel);
