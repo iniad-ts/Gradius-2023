e31ca8d6a43b8f7aa176a8a102803913c082ed84
mst-mkt <s1f102303057@iniad.org>
2023-08-17 17:14:44 +0900

    Revert "Merge branch 'main' of github.com:mst-mkt/gradius into mst-mkt"
    This reverts commit 4f3d06e75e8edcac8d852b7f2a5158a7dba319f5, reversing
    changes made to bfc91156cc2c34ae9654d1edb8b960f12c30e411.

--- src/pages/game/index.page.tsx
+++ src/pages/game/index.page.tsx
@@ -1,61 +1,51 @@
 import type { EnemyModel, PlayerModel } from '$/commonTypesWithClient/models';
 import type { BulletModelWithPosition } from '$/usecase/bulletUseCase';
-import { useEffect, useState } from 'react';
-import { Circle, Layer, Stage, Wedge } from 'react-konva';
-import { apiClient } from 'src/utils/apiClient';
+import { useState } from 'react';
+import { Layer, Stage } from 'react-konva';
 
 const Game = () => {
   const [players, setPlayers] = useState<PlayerModel[]>([]);
   const [enemies, setEnemies] = useState<EnemyModel[]>([]);
   const [bullets, setBullets] = useState<BulletModelWithPosition[]>([]);
 
-  const fetchPlayers = async () => {
-    const res = await apiClient.player.$get();
-    if (res !== null) {
-      setPlayers(res);
-    }
-  };
-
-  const fetchEnemies = async () => {
-    const res = await apiClient.enemy.$get();
-    if (res !== null) {
-      setEnemies(res);
-    }
-  };
-
-  const fetchBullets = async () => {
-    const res = await apiClient.bullet.$get();
-    if (res !== null) {
-      setBullets(res);
-    }
-  };
-  useEffect(() => {
-    const cancelId = requestAnimationFrame(() => {
-      fetchPlayers();
-      fetchEnemies();
-      fetchBullets();
-    });
-    return () => cancelAnimationFrame(cancelId);
-  });
+  // const fetchPosition = async () => {
+  //   const res = await apiClient.player.$get();
+  //   if (res !== null) {
+  //     setmyPosition(res);
+  //   }
+  // };
+  // const fetchbulletPosition = async () => {
+  //   const res = await apiClient.player.bullet.$get();
+  //   if (res !== null) {
+  //     setbulletPosition(res);
+  //   }
+  // };
+  // useEffect(() => {
+  //   const cancelId = requestAnimationFrame(() => {
+  //     console.log('fetch');
+  //     fetchPosition();
+  //     fetchbulletPosition();
+  //   });
+  //   return () => cancelAnimationFrame(cancelId);
+  // });
 
   return (
     <div>
       <Stage width={1000} height={700}>
         <Layer>
-          {players.map((player) => (
-            <Wedge
-              fill="red"
-              angle={60}
-              radius={player.radius}
-              rotation={150}
-              x={player.position.x}
-              y={player.position.y}
-              key={player.id}
-            />
-          ))}
+          {/* mapで回す */}
+
+          {/* <Wedge
+            fill="red"
+            angle={60}
+            radius={70}
+            rotation={150}
+            x={myPosition.x}
+            y={myPosition.y}
+          /> */}
         </Layer>
         <Layer>
-          {bullets.map((bullet) => (
+          {/* {bulletPosition.map((bullet) => (
             <Circle
               fill="red"
               radius={bullet.radius}
@@ -63,18 +53,12 @@ const Game = () => {
               y={bullet.position.y}
               key={bullet.id}
             />
-          ))}
+          ))} */}
         </Layer>
         <Layer>
-          {enemies.map((enemy) => (
-            <Circle
-              fill="blue"
-              radius={enemy.radius}
-              x={enemy.position.x}
-              y={enemy.position.y}
-              key={enemy.id}
-            />
-          ))}
+          {/* mapで回す */}
+
+          {/* <Circle fill="blue" x={enemyPosition.x} y={enemyPosition.y} radius={} /> */}
         </Layer>
       </Stage>
     </div>
