de559d192c86b04d2a2290c2e3386e2c15b531f3
mst-mkt <s1f102303057@iniad.org>
2023-08-17 16:50:10 +0900

    Revert "fix: apiのエンドポイント名と引数の型を修正"
    This reverts commit 073a0e747a5dc3fbb01c81f8775bac688eafe913.

--- src/pages/controller/index.page.tsx
+++ src/pages/controller/index.page.tsx
@@ -32,7 +32,7 @@ const Controller = () => {
   };
 
   const move = async () => {
-    // await apiClient.player.$post({ moveTo: moveDirection });
+    // await apiClient.move.$post({ direction: moveDirection });
     console.log('move', moveDirection.current);
   };
 
@@ -48,8 +48,8 @@ const Controller = () => {
 
   const handleMove = (e: IJoystickUpdateEvent) => {
     const moveTo = {
-      toX: Math.round(e.x ?? 0),
-      toY: Math.round(e.y ?? 0),
+      x: Math.round(e.x ?? 0),
+      y: Math.round(e.y ?? 0),
     };
     moveDirection.current = moveTo;
   };
