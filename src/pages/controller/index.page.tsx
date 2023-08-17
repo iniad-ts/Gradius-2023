cdacf0257b396d8ace0a88ba2b59409c1c31ee26
1F10220146 <s1f102201469@iniad.org>
2023-08-17 18:20:47 +0900

    Revert "はじく"
    This reverts commit 2221904d448a36b23a5726a6f3bb3cdf95574f31.

--- src/pages/controller/index.page.tsx
+++ src/pages/controller/index.page.tsx
@@ -1,3 +1,4 @@
+import type { MoveDirection } from '$/usecase/playerUsecase';
 import { useAtom } from 'jotai';
 import { userAtom } from 'src/atoms/user';
 import { Loading } from 'src/components/Loading/Loading';
@@ -8,24 +9,19 @@ const Home = () => {
   const [user] = useAtom(userAtom);
   if (!user) return <Loading visible />;
 
-  const isValidInput = (pushed: string): pushed is 'up' | 'left' | 'right' | 'down' | 'push' => {
-    return ['up', 'left', 'right', 'down', 'push'].includes(pushed);
-  };
-
   const pushButton = async (pushed: string) => {
-    if (isValidInput(pushed)) {
-      const input = pushed;
-      const res = await apiClient.rooms.control.$post({ body: input });
-      console.log(res);
-    }
+    const input = pushed as MoveDirection;
+    const res = await apiClient.rooms.control.$post({ body: input });
+    console.log(res);
   };
 
   return (
     <>
       <div className={styles.container}>
         {/* 下記は簡易的に作ったモノです。削除してもらってかまいません */}
-        {/* <h1 className={styles.word}>ここはcontrollerです</h1> */}
-        <button onClick={() => pushButton('up')}>UP</button>
+        <h1 className={styles.word}>ここはcontrollerです</h1>
+        <button onClick={() => pushButton('up')}>kakiku</button>
+        <button onClick={() => pushButton('push')}>sasisu</button>
       </div>
     </>
   );
