d15d74e23bb3b1fcc8629706395eb66889238c35
1F10220146 <s1f102201469@iniad.org>
2023-08-20 17:54:45 +0900

    Revert "player移動処理の変更"
    This reverts commit fd3545717c33998b47bb00cd2b78c035b388b179.

--- server/repository/Usecase/playerUsecase.ts
+++ server/repository/Usecase/playerUsecase.ts
@@ -1,3 +1,4 @@
+//これが必要かは微妙
 export type MoveDirection = 'up' | 'down' | 'left' | 'right';
 
 export type GameState = 'wait_start' | 'playing' | 'stop' | 'gameover' | 'clear';
@@ -10,22 +11,47 @@ export const change_state = {
   },
 };
 
-export let player_now_position: number[] = [100, 300];
+type Player_Info = {
+  pos: { x: number; y: number };
+  speed: number;
+  hp: number;
+  scole: number;
+  // userid: UserId;
+};
+
+//仮初期値
+//playerを画面の左真ん中からスタートさせるには以下の初期値が適切
+//＊stageサイズによって変更する必要在り
+//変数にして自動で調節するようにしてもいいかも
+const player_first_pos: number[] = [100, 300];
+const player_speed = 10;
+const player_hp = 100;
+const player_scole = 0;
+
+export const player_info: Player_Info = {
+  pos: { x: player_first_pos[0], y: player_first_pos[1] },
+  speed: player_speed,
+  hp: player_hp,
+  scole: player_scole,
+};
+
+export const player_now_position: number[] = [100, 300];
+
+//配列を直で渡せばこの処理自体いらない,要修正
 export const playerUsecase = {
   moveplayer: async (movedirection: MoveDirection) => {
     if (movedirection === 'up') {
-      player_now_position = [player_now_position[0] + 0, player_now_position[1] - 10];
-      return [0, -10];
+      player_info.pos.y = player_info.pos.y - player_info.speed;
+      return;
     } else if (movedirection === 'down') {
-      player_now_position = [player_now_position[0] + 0, player_now_position[1] + 10];
-      return [0, 10];
+      player_info.pos.y = player_info.pos.y + player_info.speed;
+      return;
     } else if (movedirection === 'left') {
-      player_now_position = [player_now_position[0] - 10, player_now_position[1]];
-      return [-10, 0];
+      player_info.pos.x = player_info.pos.x - player_info.speed;
+      return;
     } else if (movedirection === 'right') {
-      player_now_position = [player_now_position[0] + 10, player_now_position[1]];
-      return [10, 0];
+      player_info.pos.x = player_info.pos.x + player_info.speed;
+      return;
     }
-    return [0, 0];
   },
 };
