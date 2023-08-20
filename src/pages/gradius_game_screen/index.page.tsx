a50b4acce58868d14165179b73cd0329b7ccff35
1F10220146 <s1f102201469@iniad.org>
2023-08-20 18:03:35 +0900

    Revert "Merge pull request #11 from s1f10230014/eguchijun"
    This reverts commit 8d196017b55c6017fa64bf9fc0a8ad6ecf6a6d7e, reversing
    changes made to 8fe4ccbf1a32bd979f92d09e31aeea50f1ce4e20.

--- src/pages/gradius_game_screen/index.page.tsx
+++ src/pages/gradius_game_screen/index.page.tsx
@@ -9,33 +9,19 @@ import { apiClient } from 'src/utils/apiClient';
 const App = () => {
   const [fight_position, setfight_position] = useState([0, 0]);
   const [enemies, setenemies] = useState<number[][]>([]);
-  const [isFighterLoaded, setIsFighterLoaded] = useState(false);
-  // const fighterImg = new window.Image();
-  // fighterImg.src = '../../../public/images/fighter.png';
-  const fighterImgRef = useRef(new window.Image());
-
   const fetchBord = async () => {
-    const newFighterPosition = await apiClient.game_screen.$get();
-    const newEnemyPos = await apiClient.enemy.$get();
-    setfight_position(newFighterPosition);
-    setenemies(newEnemyPos);
+    const new_fighter_position = await apiClient.game_screen.$get();
+    const new_enemy_pos = await apiClient.enemy.$get();
+    setfight_position(new_fighter_position);
+    setenemies(new_enemy_pos);
   };
-
-  useEffect(() => {
-    fighterImgRef.current.src = '/images/fighter.png'; // 画像ファイルの相対パスを指定
-    fighterImgRef.current.onload = () => {
-      setIsFighterLoaded(true);
-    };
-  }, []);
-
   useEffect(() => {
     const cancellid = setInterval(fetchBord, 100);
     return () => {
       clearInterval(cancellid);
     };
   }, []);
-
-  if (!fight_position || !isFighterLoaded) return <Loading visible />;
+  if (!fight_position) return <Loading visible />;
   return (
     <>
       <Stage width={1100} height={690}>
@@ -47,29 +33,9 @@ const App = () => {
             x={fight_position[0]}
             y={fight_position[1]}
           />
-          {/* <img src={fighter.src} width={fighter.width} height={fighter.height} /> */}
-          <Image
-            image={fighterImgRef.current}
-            width={fighterImgRef.current.width}
-            height={fighterImgRef.current.height}
-          />
-          {enemies.map((enemy, index) => (
-            <Rect
-              key={index}
-              id={`enemy_${index}`}
-              fill="black"
-              width={40}
-              height={40}
-              x={enemy[0]}
-              y={enemy[1]}
-            />
-          ))}
-        </Layer>
-        <Layer>
-          <Rect id="enemy01" stroke="black" strokeWidth={1} />
-        </Layer>
-      </Stage>
-    </>
+        ))}
+      </Layer>
+    </Stage>
   );
 };
 export default App;
