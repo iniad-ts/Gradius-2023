e160cd83cd1457a3456d339e98f102f56a4f0b6b
1F10220146 <s1f102201469@iniad.org>
2023-08-20 18:04:51 +0900

    Revert "player動かない"
    This reverts commit 213f5f6a3bbf1b639b5090773cbd2289807c129e.

--- src/pages/gradius_game_screen/index.page.tsx
+++ src/pages/gradius_game_screen/index.page.tsx
@@ -39,7 +39,7 @@ const App = () => {
     };
   }, []);
 
-  if (!isFighterLoaded) return <Loading visible />;
+  if (!fight_position || !isFighterLoaded) return <Loading visible />;
   return (
     <>
       <Stage width={1100} height={690}>
