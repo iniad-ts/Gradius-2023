835cc49c9e4b6c204befb9ddac38bf1d721e7fb3
1F10220146 <s1f102201469@iniad.org>
2023-08-20 17:54:57 +0900

    Revert "Merge pull request #13 from s1f10230014/shogo"
    This reverts commit 28e31899add917e0b81ee51f11d438a2f831a658, reversing
    changes made to 37fde2a129f798b0b7df8518aae9acd177222712.

--- src/pages/gradius_game_screen/index.page.tsx
+++ src/pages/gradius_game_screen/index.page.tsx
@@ -1,22 +1,19 @@
 //ここにゲーム画面をつくる
-import type { Enemy_Info } from '$/repository/Usecase/enemyUsecase';
-import type { Laser_Info } from '$/repository/Usecase/laserUsecase';
 import { useEffect, useState } from 'react';
 import { Layer, Rect, Stage, Wedge } from 'react-konva';
 import { Loading } from 'src/components/Loading/Loading';
 import { apiClient } from 'src/utils/apiClient';
 const App = () => {
   const [fight_position, setfight_position] = useState<number[]>();
-  const [enemieies_info, setenemieies_info] = useState<Enemy_Info[]>([]);
-  const [laseies_info, setlaseies_info] = useState<Laser_Info[]>([]);
-
+  const [enemies, setenemies] = useState<number[][]>([]);
+  const [laser_pos, setlaser_pos] = useState<number[][]>([]);
   const fetchBord = async () => {
     const new_fighter_position = await apiClient.player.$get();
     const new_enemy_pos = await apiClient.enemy.$get();
     const new_laser_pos = await apiClient.laser.$get();
     setfight_position(new_fighter_position);
-    setenemieies_info(new_enemies_info);
-    setlaseies_info(new_laseies_info);
+    setenemies(new_enemy_pos);
+    setlaser_pos(new_laser_pos);
   };
   console.log(laser_pos);
   useEffect(() => {
@@ -38,15 +35,15 @@ const App = () => {
           x={fight_position[0]}
           y={fight_position[1]}
         />
-        {enemieies_info.map((enemy, index) => (
+        {enemies.map((enemy, index) => (
           <Rect
             key={index}
             id={`enemy_${index}`}
             fill="black"
             width={40}
             height={40}
-            x={enemy.pos.x}
-            y={enemy.pos.y}
+            x={enemy[0]}
+            y={enemy[1]}
           />
         ))}
         {laser_pos.map((laser, index) => (
