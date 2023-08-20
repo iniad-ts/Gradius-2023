4dd456e3176208fc7c55aebae41b06b5364c5a1f
1F10220146 <s1f102201469@iniad.org>
2023-08-21 03:08:50 +0900

    fix: findAll()で常に配列を返すように

--- server/api/player/controller.ts
+++ server/api/player/controller.ts
@@ -2,5 +2,5 @@ import { playerRepository } from '$/repository/playerRepository';
 import { defineController } from './$relay';
 
 export default defineController(() => ({
-  get: async () => ({ status: 200, body: await playerRepository.findAll() }),
+  get: async () => ({ status: 200, body: (await playerRepository.findAll()) ?? [] }),
 }));
