c73c273f845cc40b7ce267a867686bba796c4e4f
1F10220146 <s1f102201469@iniad.org>
2023-08-20 18:01:48 +0900

    Revert "ERROR: lint:prettier exited with 1."
    This reverts commit 0fcd7b43a5e68bbe41e77d5520c0a17f8c23a7e9.

---
+++ server/api/controller/player/controller.ts
@@ -0,0 +1,12 @@
+import { playerUsecase } from '$/repository/Usecase/playerUsecase';
+import { defineController } from './$relay';
+
+export default defineController(() => ({
+  post: async ({ body }) => {
+    const result = await playerUsecase.moveplayer(body);
+    return {
+      status: 202,
+      body: result,
+    };
+  },
+}));
