85f3dee820e320a88693392a23dd041e2568cb61
enbipaipu <s1f102300122@iniad.org>
2023-08-27 00:38:10 +0900

    enemy関係を作った

--- server/usecase/enemyUsecase.ts
+++ server/usecase/enemyUsecase.ts
@@ -3,7 +3,19 @@ import { enemyRepository } from '$/repository/enemyRepository';
 import { enemyIdParser } from '$/service/idParsers';
 import { randomUUID } from 'crypto';
 
+let intervalId: NodeJS.Timeout | null = null;
 export const enemyUsecase = {
+  init() {
+    intervalId = setInterval(() => {
+      enemyUsecase.update();
+    }, 500);
+  },
+  stop() {
+    if (intervalId) {
+      clearInterval(intervalId);
+      intervalId = null;
+    }
+  },
   create: async (): Promise<EnemyModel | null> => {
     const enemyData: EnemyModel = {
       enemyId: enemyIdParser.parse(randomUUID()),
@@ -18,5 +30,35 @@ export const enemyUsecase = {
   findAll: async (): Promise<EnemyModel[]> => {
     const enemies = await enemyRepository.findAll();
     return enemies.length > 0 ? enemies : [];
+  },
+  move: async (enemyModel: EnemyModel): Promise<EnemyModel | null> => {
+    const currentEnemyInfo = await enemyRepository.find(enemyModel.enemyId);
+    if (currentEnemyInfo === null) return null;
+    const updateEnemyInfo: EnemyModel = {
+      ...currentEnemyInfo,
+      pos: {
+        x: currentEnemyInfo.pos.x - currentEnemyInfo.vector.x,
+        y: currentEnemyInfo.pos.y - currentEnemyInfo.vector.y,
+      },
+    };
+    await enemyRepository.save(updateEnemyInfo);
+    return updateEnemyInfo;
+  },
+  delete: async (enemyModel: EnemyModel): Promise<EnemyModel | null> => {
+    const currentEnemyInfo = await enemyRepository.find(enemyModel.enemyId);
+    if (currentEnemyInfo === null) return null;
+    await enemyRepository.delete(enemyModel.enemyId);
+    return currentEnemyInfo;
+  },
+  update: async () => {
+    const currentEnemyInfos = await enemyRepository.findAll();
+    const promises = currentEnemyInfos.map((enemy) => {
+      if (enemy.pos.x > 1920 || enemy.pos.y < 0) {
+        return enemyUsecase.delete(enemy);
+      } else {
+        return enemyUsecase.move(enemy);
+      }
+    });
+    await Promise.all(promises);
   },
 };
