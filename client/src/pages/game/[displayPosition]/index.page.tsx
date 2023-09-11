b7e9127929d6f81acd15896dcbf790b061dcd7d1
imoken777 <s1f102301640@iniad.org>
2023-09-11 12:27:01 +0900

    playerに名前を表示

--- client/src/pages/game/[displayPosition]/index.page.tsx
+++ client/src/pages/game/[displayPosition]/index.page.tsx
@@ -1,8 +1,8 @@
 import { ENEMY_HALF_WIDTH, SCREEN_HEIGHT, SCREEN_WIDTH } from 'commonConstantsWithClient';
 import type { BulletModel, EnemyModel, PlayerModel } from 'commonTypesWithClient/models';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
-import { Image, Layer, Stage } from 'react-konva';
+import React, { useEffect, useState } from 'react';
+import { Image, Layer, Stage, Text } from 'react-konva';
 import Boom from 'src/components/Effect/Boom';
 import { Bullet } from 'src/components/Entity/Bullet';
 import { Enemy } from 'src/components/Entity/Enemy';
@@ -140,7 +140,10 @@ const Game = () => {
         </Layer>
         <Layer>
           {players.map((player) => (
-            <Player displayPosition={displayPosition ?? 0} player={player} key={player.id} />
+            <React.Fragment key={player.id}>
+              <Text x={player.pos.x} y={player.pos.y - 80} text={player.name} fontSize={30} />
+              <Player displayPosition={displayPosition ?? 0} player={player} />
+            </React.Fragment>
           ))}
         </Layer>
         <Layer>
