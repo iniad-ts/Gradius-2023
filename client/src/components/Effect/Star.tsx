5468ecf93f258cc45eb2d25441578879775f239a
1f10230234 <s1f102302348@iniad.org>
2023-09-20 21:19:06 +0900

    fix 点滅方法を透明度西、色を追加

--- client/src/components/Effect/Star.tsx
+++ client/src/components/Effect/Star.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
 import { Circle } from 'react-konva';
 import type { Pos } from 'src/types/types';
+import { hslToRGB } from 'src/utils/hslToRGB';
 
 type MeteorModel = {
   pos: Pos;
   createdAt: number;
   duration: number;
+  color: string;
 };
 
 export const Star = () => {
@@ -17,24 +19,39 @@ export const Star = () => {
     if (star === null) {
       const x = Math.floor(Math.random() * 1920);
       const y = Math.floor(Math.random() * 540);
-      const duration = Math.random() * 2000;
+      const duration = 3000 + Math.random() * 2000;
+      const h = Math.random() * 90;
+      const fixedH = h > 70 ? h + 100 : h;
+      const color = hslToRGB(fixedH, 1, 0.5);
       setStar({
         pos: { x, y },
         duration,
         createdAt: Date.now(),
+        color,
       });
       setTimeout(() => setStar(null), duration);
     }
   }, [star]);
 
-  if (star === null) return false;
+  if (star === null)
+    return (
+      <Circle
+        x={Math.floor(Math.random() * 1920)}
+        y={Math.floor(Math.random() * 540)}
+        radius={1}
+        fill="#fff"
+      />
+    );
 
   return (
     <Circle
       x={star.pos.x}
       y={star.pos.y}
-      radius={6 - Math.abs((600 - (time % star.duration)) / 100)}
-      fill="#fff"
+      radius={5}
+      fill={star.color}
+      opacity={
+        1 - Math.abs(star.duration - ((time - star.createdAt) % star.duration) * 2) / star.duration
+      }
     />
   );
 };
