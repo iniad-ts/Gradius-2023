7786bb78470df04f44c62a8d5e6de883d12144a3
1f10230234 <s1f102302348@iniad.org>
2023-09-21 04:09:36 +0900

    fix 短絡しやすく

--- server/usecase/@service/isOtherSide.ts
+++ server/usecase/@service/isOtherSide.ts
@@ -1,7 +1,7 @@
 import type { EntityModel } from '../../commonTypesWithClient/models';
 
 export const isOtherSide = (target1: EntityModel, target2: EntityModel) => {
-  if ('side' in target1 && 'score' in target1 && 'side' in target2 && 'score' in target2) {
+  if ('side' in target1 && 'side' in target2 && 'score' in target1 && 'score' in target2) {
     return target1.side !== target2.side;
   }
   return true;
