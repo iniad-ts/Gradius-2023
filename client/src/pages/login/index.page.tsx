ecb6fe76c3a2052decd2ef062855c4c1ac038f8d
1f10230234 <s1f102302348@iniad.org>
2023-09-21 12:40:55 +0900

    add デバッグ用書き換え

--- client/src/pages/login/index.page.tsx
+++ client/src/pages/login/index.page.tsx
@@ -2,12 +2,30 @@ import type { PlayerModel } from 'commonTypesWithClient/models';
 import { useRouter } from 'next/router';
 import type { ChangeEvent } from 'react';
 import { useCallback, useEffect, useState } from 'react';
+import { DisplayYourSide } from 'src/components/DisplayYourSide/DisplayYourSide';
 import { apiClient } from 'src/utils/apiClient';
 import { getUserIdFromLocalStorage, loginWithLocalStorage } from 'src/utils/loginWithLocalStorage';
+import { userIdParser } from '../../../../server/service/idParsers';
 import styles from './index.module.css';
+
+const player: PlayerModel = {
+  id: userIdParser.parse('aaa'),
+  name: 'yossuli',
+  pos: { x: 0, y: 540 },
+  score: 0,
+  Items: [],
+  side: 'left',
+  isPlaying: true,
+  speed: 1,
+  startedAt: 0,
+  usingItem: 'a',
+};
+
+const DISPLAY_DURATION = 5000;
 
 const Login = () => {
   const [name, setName] = useState<string>('');
+  const [hoge, setHoge] = useState(true);
   const router = useRouter();
 
   const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
@@ -35,22 +53,26 @@ const Login = () => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.titlecard}>
-        <h1 className={styles.title}>Gradius</h1>
-        <span />
-        <p className={styles.announcement}>ニックネームを入力してください</p>
-        <input
-          type="text"
-          placeholder="ここに入力"
-          className={styles.input}
-          value={name}
-          onChange={handleInput}
-        />
-        <button className={styles.button} disabled={name === ''} onClick={login}>
-          プレイ
-        </button>
-        <p className={styles.announcement}>※ニックネームはゲームに使用されます</p>
-      </div>
+      {hoge ? (
+        <div className={styles.titlecard}>
+          <h1 className={styles.title}>Gradius</h1>
+          <span />
+          <p className={styles.announcement}>ニックネームを入力してください</p>
+          <input
+            type="text"
+            placeholder="ここに入力"
+            className={styles.input}
+            value={name}
+            onChange={handleInput}
+          />
+          <button className={styles.button} disabled={name === ''} onClick={() => setHoge(false)}>
+            プレイ
+          </button>
+          <p className={styles.announcement}>※ニックネームはゲームに使用されます</p>
+        </div>
+      ) : (
+        <DisplayYourSide player={player} displayDuration={DISPLAY_DURATION} router={router} />
+      )}
     </div>
   );
 };
